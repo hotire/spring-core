dd2a6230f8c2a4ca7baf94e4d779ca2ff482fede
user <gngh0101@gmail.com>
2019-11-22 19:39:24 +0900

    Upgrade guava 28.0-jre

--- src/main/java/com/kakao/hotire/springcore/controller/SimpleRestController.java
+++ src/main/java/com/kakao/hotire/springcore/controller/SimpleRestController.java
@@ -1,14 +1,29 @@
 package com.kakao.hotire.springcore.controller;
 
+import com.kakao.hotire.springcore.bean.Proto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleRestController {
 
+  @Autowired(required = false)
+  Proto proto;
+
+  @Autowired
+  SimpleService simpleService;
+
   @GetMapping("/hello3")
   public String hello() {
     return "hello3";
+  }
+
+  @GetMapping("/hello4")
+  public String h() {
+    System.out.println(proto);
+    simpleService.test();
+    return proto.toString();
   }
 
 }
