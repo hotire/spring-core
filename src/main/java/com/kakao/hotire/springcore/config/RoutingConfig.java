1d24e7e5ddc57f0aa9a960450e0dc2b24a7e8365
user <gngh0101@gmail.com>
2019-12-23 00:17:38 +0900

    Optimize imports

--- src/main/java/com/kakao/hotire/springcore/config/RoutingConfig.java
+++ src/main/java/com/kakao/hotire/springcore/config/RoutingConfig.java
@@ -1,11 +1,7 @@
 package com.kakao.hotire.springcore.config;
 
 import com.kakao.hotire.springcore.controller.SimpleController;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
