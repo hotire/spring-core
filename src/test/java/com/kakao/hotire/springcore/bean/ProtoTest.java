3026f4c91518b8374c6eb1c803a9b0dfc366d4b7
user <gngh0101@gmail.com>
2019-12-23 00:18:25 +0900

    Optimize imports

--- src/test/java/com/kakao/hotire/springcore/bean/ProtoTest.java
+++ src/test/java/com/kakao/hotire/springcore/bean/ProtoTest.java
@@ -3,7 +3,6 @@ package com.kakao.hotire.springcore.bean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
