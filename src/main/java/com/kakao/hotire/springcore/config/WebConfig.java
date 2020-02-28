06c76e82c2e47a90f8483fdade5f160c895ccf21
hotire <gngh0101@gmail.com>
2020-02-28 23:37:11 +0900

    Fix converter to be used utf-8

--- src/main/java/com/kakao/hotire/springcore/config/WebConfig.java
+++ src/main/java/com/kakao/hotire/springcore/config/WebConfig.java
@@ -1,8 +1,13 @@
 package com.kakao.hotire.springcore.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -10,5 +15,13 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
     registry.addViewController("/simple2").setViewName("/index");
+  }
+
+  @Override
+  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    converters.stream()
+              .filter(it -> it instanceof AbstractHttpMessageConverter)
+              .map(AbstractHttpMessageConverter.class::cast)
+              .forEach(it -> it.setDefaultCharset(StandardCharsets.UTF_8));
   }
 }
