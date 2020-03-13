59be7c621269627d24a82507a99d428873ecb4d6
hotire <gngh0101@gmail.com>
2020-03-13 23:29:50 +0900

    Add getter

--- src/main/java/com/kakao/hotire/springcore/argument/Entity.java
+++ src/main/java/com/kakao/hotire/springcore/argument/Entity.java
@@ -1,7 +1,9 @@
 package com.kakao.hotire.springcore.argument;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public class Entity {
     private final Long id;
