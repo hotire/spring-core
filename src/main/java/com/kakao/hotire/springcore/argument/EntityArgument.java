5dc643d8943b4c075b787558bb94dce4bb70229e
hotire <gngh0101@gmail.com>
2020-03-13 22:53:36 +0900

    Add custom argument annotation EntityArgument

---
+++ src/main/java/com/kakao/hotire/springcore/argument/EntityArgument.java
@@ -0,0 +1,12 @@
+package com.kakao.hotire.springcore.argument;
+
+
+import java.lang.annotation.*;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface EntityArgument {
+    EntityArgumentResolver.EntityType type();
+    String value() default "";
+}
