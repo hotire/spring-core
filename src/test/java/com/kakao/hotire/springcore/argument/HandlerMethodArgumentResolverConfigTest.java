18aa48946b92d13a02a422917b379ac83efcbcae
hotire <gngh0101@gmail.com>
2020-03-17 23:45:29 +0900

    Verify addArgumentResolvers

--- src/test/java/com/kakao/hotire/springcore/argument/HandlerMethodArgumentResolverConfigTest.java
+++ src/test/java/com/kakao/hotire/springcore/argument/HandlerMethodArgumentResolverConfigTest.java
@@ -3,6 +3,9 @@ package com.kakao.hotire.springcore.argument;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class HandlerMethodArgumentResolverConfigTest {
@@ -17,5 +20,19 @@ class HandlerMethodArgumentResolverConfigTest {
 
         // then
         assertThat(entityArgumentResolver).isInstanceOf(HandlerMethodArgumentResolver.class);
+    }
+
+    @Test
+    void addArgumentResolvers() {
+        // given
+        final List<HandlerMethodArgumentResolver> resolvers = new ArrayList<>();
+        final HandlerMethodArgumentResolverConfig config = new HandlerMethodArgumentResolverConfig();
+
+        // when
+        config.addArgumentResolvers(resolvers);
+
+        // then
+        assertThat(resolvers.size()).isEqualTo(1);
+        assertThat(resolvers.get(0)).isInstanceOf(EntityArgumentResolver.class);
     }
 }
