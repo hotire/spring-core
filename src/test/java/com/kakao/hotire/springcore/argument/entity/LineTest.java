a44c7a48f6c7b36180a7df7220ebf79e77d69831
hotire <gngh0101@gmail.com>
2020-03-20 00:53:21 +0900

    Verify create line entity

---
+++ src/test/java/com/kakao/hotire/springcore/argument/entity/LineTest.java
@@ -0,0 +1,14 @@
+package com.kakao.hotire.springcore.argument.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LineTest {
+
+    @Test
+    void create() {
+        // when then
+        assertThat(new Line(1L));
+    }
+}
