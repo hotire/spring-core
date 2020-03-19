d0914f75b0556d4a7413f4e1d80f3f2d51314d1d
hotire <gngh0101@gmail.com>
2020-03-20 00:52:56 +0900

    Verify create kakao entity

---
+++ src/test/java/com/kakao/hotire/springcore/argument/entity/KakaoTest.java
@@ -0,0 +1,14 @@
+package com.kakao.hotire.springcore.argument.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class KakaoTest {
+
+    @Test
+    void create() {
+        // when then
+        assertThat(new Kakao(1L));
+    }
+}
