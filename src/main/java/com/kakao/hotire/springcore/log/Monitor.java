8c62a728ca5d5d01020043d5695cdea2515b476f
hotire <gngh0101@gmail.com>
2020-04-15 02:23:55 +0900

    Add Inherited annoation

--- src/main/java/com/kakao/hotire/springcore/log/Monitor.java
+++ src/main/java/com/kakao/hotire/springcore/log/Monitor.java
@@ -1,10 +1,8 @@
 package com.kakao.hotire.springcore.log;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface Monitor {
