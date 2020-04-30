43000a1e8a79ecf89250ab77e035cd109f86755b
hotire <gngh0101@gmail.com>
2020-05-01 00:10:45 +0900

    Add final to variable

--- src/test/java/com/kakao/hotire/springcore/bean/ProtoTest.java
+++ src/test/java/com/kakao/hotire/springcore/bean/ProtoTest.java
@@ -1,8 +1,6 @@
 package com.kakao.hotire.springcore.bean;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -37,13 +37,13 @@ public class ProtoTest {
 
   @Test
   public void assert_singleton() {
-    Single single = context.getBean(Single.class);
+    final Single single = context.getBean(Single.class);
     assertThat(single).isEqualTo(single);
   }
 
   @Test
   public void assert_prototype() {
-    Proto proto = context.getBean(Proto.class);
+    final Proto proto = context.getBean(Proto.class);
     assertThat(proto).isNotEqualTo(context.getBean(Proto.class));
   }
 
