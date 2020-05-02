93e92b77a92e27f011577760bcd63ac123512a22
hotire <gngh0101@gmail.com>
2020-05-02 15:29:14 +0900

    Using JUnit5 instead of JUnit4

--- src/test/java/com/kakao/hotire/springcore/bean/ProtoTest.java
+++ src/test/java/com/kakao/hotire/springcore/bean/ProtoTest.java
@@ -1,50 +1,44 @@
 package com.kakao.hotire.springcore.bean;
 
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProtoTest {
-
-  @Configuration
-  static class Config {
-    @Bean
-    public Single single() {
+    @Configuration
+    static class Config {
+        @Bean
+        public Single single() {
       return new Single();
     }
 
-    @Scope("prototype")
-    @Bean
-    public Proto proto() {
+        @Scope("prototype")
+        @Bean
+        public Proto proto() {
       return new Proto();
     }
-  }
+    }
 
-  @Autowired
-  private ApplicationContext context;
+    @Autowired
+    private ApplicationContext context;
 
+    @Test
+    void assert_singleton() {
+        final Single single = context.getBean(Single.class);
+        assertThat(single).isEqualTo(single);
+    }
 
-  @Test
-  public void assert_singleton() {
-    final Single single = context.getBean(Single.class);
-    assertThat(single).isEqualTo(single);
-  }
-
-  @Test
-  public void assert_prototype() {
-    final Proto proto = context.getBean(Proto.class);
-    assertThat(proto).isNotEqualTo(context.getBean(Proto.class));
-  }
-
+    @Test
+    void assert_prototype() {
+        final Proto proto = context.getBean(Proto.class);
+        assertThat(proto).isNotEqualTo(context.getBean(Proto.class));
+    }
 }
