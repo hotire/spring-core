d17ccd525662f4e29259110fc6762a51f56f0df7
hotire <gngh0101@gmail.com>
2020-05-03 00:23:24 +0900

    Using JUnit5 instead of JUnit4

--- src/test/java/com/kakao/hotire/springcore/bean/ScopeProxyTest.java
+++ src/test/java/com/kakao/hotire/springcore/bean/ScopeProxyTest.java
@@ -1,9 +1,6 @@
 package com.kakao.hotire.springcore.bean;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -11,27 +8,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-public class ScopeProxyTest {
+class ScopeProxyTest {
 
-  @Configuration
-  static class Config {
-    @Scope(value = "prototype", proxyMode = ScopedProxyMode.INTERFACES)
-    @Bean
-    public Proto proto() {
+    @Configuration
+    static class Config {
+        @Scope(value = "prototype", proxyMode = ScopedProxyMode.INTERFACES)
+        @Bean
+        public Proto proto() {
       return new Proto();
     }
-  }
+    }
 
-  @Autowired
-  private ApplicationContext context;
+    @Autowired
+    private ApplicationContext context;
 
-  @Test
-  public void assert_prototype() {
-    Proto proto = context.getBean(Proto.class);
-    assertThat(proto).isEqualTo(context.getBean(Proto.class));
-  }
+    @Test
+    void assert_prototype() {
+        // when
+        final Proto proto = context.getBean(Proto.class);
+        // then
+        assertThat(proto).isEqualTo(context.getBean(Proto.class));
+    }
 }
