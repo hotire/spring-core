5f742fec30b5766edc8fd6b2b48798a99951353c
hotire <gngh0101@gmail.com>
2020-07-16 01:19:26 +0900

    Add EventDataJpaTest

---
+++ src/test/java/com/kakao/hotire/springcore/event/EventDataJpaTest.java
@@ -0,0 +1,34 @@
+package com.kakao.hotire.springcore.event;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@DataJpaTest
+public @interface EventDataJpaTest {
+
+    @AliasFor(annotation = DataJpaTest.class)
+    String[] properties() default {};
+
+    @AliasFor(annotation = DataJpaTest.class)
+    boolean showSql() default true;
+
+    @AliasFor(annotation = DataJpaTest.class)
+    boolean useDefaultFilters() default true;
+
+    @AliasFor(annotation = DataJpaTest.class)
+    ComponentScan.Filter[] includeFilters() default {};
+
+    @AliasFor(annotation = DataJpaTest.class)
+    ComponentScan.Filter[] excludeFilters() default {};
+
+    @AliasFor(annotation = DataJpaTest.class)
+    Class<?>[] excludeAutoConfiguration() default {};
+}
