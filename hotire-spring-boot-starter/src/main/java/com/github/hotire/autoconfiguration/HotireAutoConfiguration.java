969ff819c82c586e0b0a66ed8ac4ef5bc8d393c3
hotire <gngh0101@gmail.com>
2020-07-29 01:07:13 +0900

    Fix Config to be Service

--- hotire-spring-boot-starter/src/main/java/com/github/hotire/autoconfiguration/HotireAutoConfiguration.java
+++ hotire-spring-boot-starter/src/main/java/com/github/hotire/autoconfiguration/HotireAutoConfiguration.java
@@ -1,18 +1,17 @@
 package com.github.hotire.autoconfiguration;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-@ConditionalOnClass(WebFluxConfigurer.class)
+@ConditionalOnProperty(prefix = "hotire.webflux", value = "enabled", matchIfMissing = true)
 @EnableConfigurationProperties(HotireProperties.class)
 public class HotireAutoConfiguration {
 
     @Bean
-    public HotireWebFluxConfigurer hotireWebFluxConfigurer(final HotireProperties properties) {
-        return new HotireWebFluxConfigurer(properties);
+    public HotireService hotireService(final HotireProperties properties) {
+        return new HotireService(properties);
     }
 }
--- hotire-spring-boot-starter/src/main/java/com/github/hotire/autoconfiguration/HotireService.java
+++ hotire-spring-boot-starter/src/main/java/com/github/hotire/autoconfiguration/HotireService.java
@@ -3,11 +3,10 @@ package com.github.hotire.autoconfiguration;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Slf4j
 @RequiredArgsConstructor
-public class HotireWebFluxConfigurer implements WebFluxConfigurer, InitializingBean {
+public class HotireService implements InitializingBean {
 
     private final HotireProperties properties;
 
