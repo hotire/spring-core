d80fe25802e650db48d6950bd5fc1ce870fd5b13
hotire <gngh0101@gmail.com>
2020-08-01 01:06:27 +0900

    Add final to async field

--- src/main/java/com/kakao/hotire/springcore/event/AsyncApplicationEvent.java
+++ src/main/java/com/kakao/hotire/springcore/event/AsyncApplicationEvent.java
@@ -6,9 +6,14 @@ import org.springframework.context.ApplicationEvent;
 public class AsyncApplicationEvent extends ApplicationEvent {
 
     @Getter
-    private boolean async = true;
+    private final boolean async;
 
     public AsyncApplicationEvent(Object source) {
+        this(source, true);
+    }
+
+    public AsyncApplicationEvent(Object source, final boolean async) {
         super(source);
+        this.async = async;
     }
 }
