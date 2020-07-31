b9866cd12e5c7abee83d80d77ac1b5be5a8f8dd1
hotire <gngh0101@gmail.com>
2020-08-01 01:07:19 +0900

    Verify EntityEventListener

---
+++ src/test/java/com/kakao/hotire/springcore/event/EntityEventListenerTest.java
@@ -0,0 +1,33 @@
+package com.kakao.hotire.springcore.event;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.test.context.TestConstructor;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@RequiredArgsConstructor
+class EntityEventListenerTest {
+    private final ApplicationEventPublisher publisher;
+
+    @Test
+    void consume() {
+        // given
+        final AtomicBoolean atomicBoolean = mock(AtomicBoolean.class);
+        final CallbackEvent event = new CallbackEvent(atomicBoolean);
+        event.setCallback(o -> atomicBoolean.set(true));
+
+        // when
+        publisher.publishEvent(new AsyncApplicationEvent("data"));
+
+
+        // then
+        verify(atomicBoolean, timeout(2000)).set(true);
+    }
+}
