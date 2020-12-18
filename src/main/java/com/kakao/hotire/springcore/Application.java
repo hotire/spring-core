e46f30147d75fb6e75cb6e84f765e84d20357e5f
hotire <gngh0101@gmail.com>
2020-12-19 02:52:24 +0900

    Rename application

--- src/main/java/com/kakao/hotire/springcore/Application.java
+++ src/main/java/com/kakao/hotire/springcore/Application.java
@@ -1,15 +1,16 @@
 package com.kakao.hotire.springcore;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @SpringBootApplication
-public class SpringCoreApplication {
+public class Application {
 
- public static void main(String[] args) {
-    SpringApplication.run(SpringCoreApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
