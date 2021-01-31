1acb8096da9bb56aebfebc8421850e266aff5e94
hotire <gngh0101@gmail.com>
2021-02-01 00:48:59 +0900

    Add MessageSourceConfig

---
+++ src/main/java/com/kakao/hotire/springcore/message/MessageSourceConfig.java
@@ -0,0 +1,10 @@
+package com.kakao.hotire.springcore.message;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * https://docs.spring.io/spring-framework/docs/current/reference/html/core.html#context-functionality-messagesource
+ */
+@Configuration
+public class MessageSourceConfig {
+}
