439759bd140801b63ab2bcac7ae5c97f43687375
hotire <gngh0101@gmail.com>
2021-05-10 00:06:01 +0900

    Add CustomFilterConfig

---
+++ src/main/java/com/kakao/hotire/springcore/filter/CustomFilterConfig.java
@@ -0,0 +1,34 @@
+package com.kakao.hotire.springcore.filter;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomFilterConfig {
+
+    public static class LoggingFilter implements Filter  {
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Bean
+    public FilterRegistrationBean<LoggingFilter> loggingFilter(){
+        final FilterRegistrationBean<LoggingFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new LoggingFilter());
+        bean.setUrlPatterns(List.of("*"));
+        return bean;
+    }
+
+}
