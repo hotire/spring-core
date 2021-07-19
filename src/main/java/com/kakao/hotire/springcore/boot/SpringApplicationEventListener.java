9ebc5b770353e7398d97f20620a2061b07330282
hotire <gngh0101@gmail.com>
2021-07-20 00:59:51 +0900

    Add SpringApplicationEventListener

---
+++ src/main/java/com/kakao/hotire/springcore/boot/SpringApplicationEventListener.java
@@ -0,0 +1,15 @@
+package com.kakao.hotire.springcore.boot;
+
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.context.ApplicationListener;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SpringApplicationEventListener implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
+
+    @Override
+    public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
+        log.info("SpringApplicationEventListener listen {}", event);
+    }
+}
