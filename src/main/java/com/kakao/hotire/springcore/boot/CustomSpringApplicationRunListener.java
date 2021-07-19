03f94cc43d8328f2e9fd61528e72b70643b82461
hotire <gngh0101@gmail.com>
2021-07-19 22:13:16 +0900

    Learn boot SpringApplicationRunListeners

--- src/main/java/com/kakao/hotire/springcore/boot/CustomSpringApplicationRunListener.java
+++ src/main/java/com/kakao/hotire/springcore/boot/CustomSpringApplicationRunListener.java
@@ -8,6 +8,10 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ *  @see org.springframework.boot.SpringApplicationRunListeners;
+ *  @see org.springframework.boot.context.event.EventPublishingRunListener;
+ */
 @Slf4j
 @RequiredArgsConstructor
 public class CustomSpringApplicationRunListener implements SpringApplicationRunListener {
@@ -26,12 +30,10 @@ public class CustomSpringApplicationRunListener implements SpringApplicationRunL
         log.info("environmentPrepared");
     }
 
-
     @Override
     public void contextPrepared(ConfigurableApplicationContext context) {
         log.info("contextPrepared");
     }
-
 
     @Override
     public void contextLoaded(ConfigurableApplicationContext context) {
