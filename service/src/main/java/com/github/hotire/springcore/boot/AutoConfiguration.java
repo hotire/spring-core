3848f4f6d4c5530e271957dc95bb6c82037f6ec8
hotire <gngh0101@gmail.com>
2021-07-22 01:58:11 +0900

    Learn AutoConfiguration

---
+++ service/src/main/java/com/github/hotire/springcore/boot/AutoConfiguration.java
@@ -0,0 +1,16 @@
+package com.github.hotire.springcore.boot;
+
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * EnableAutoConfiguration has AutoConfigurationPackage annoation
+ *
+ * @Import(AutoConfigurationPackages.Registrar.class)
+ *
+ * @see org.springframework.boot.autoconfigure.AutoConfigurationPackages.Registrar is ImportBeanDefinitionRegistrar
+ */
+@AutoConfigurationPackage
+@EnableAutoConfiguration
+public class AutoConfiguration {
+}
