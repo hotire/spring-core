2012f70d418599fe176eaa8bd7e965aa04f42bdc
hotire <gngh0101@gmail.com>
2021-07-23 01:03:03 +0900

    Learn AutoConfigurationPackages.get

--- service/src/main/java/com/github/hotire/springcore/boot/AutoConfiguration.java
+++ service/src/main/java/com/github/hotire/springcore/boot/AutoConfiguration.java
@@ -1,7 +1,16 @@
 package com.github.hotire.springcore.boot;
 
+import java.util.List;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * EnableAutoConfiguration has AutoConfigurationPackage annoation
@@ -13,7 +22,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
  * @see AutoConfigurationPackage
  * @see org.springframework.boot.autoconfigure.AutoConfigurationPackages.Registrar is ImportBeanDefinitionRegistrar
  */
-@AutoConfigurationPackage
-@EnableAutoConfiguration
-public class AutoConfiguration {
+@Slf4j
+@Configuration
+public class AutoConfiguration implements BeanFactoryAware {
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        final List<String> basePackages = AutoConfigurationPackages.get(beanFactory);
+        basePackages.forEach(log::info);
+    }
 }
