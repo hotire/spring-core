8e3b92c3a293110577c9ac62ae749b5744f3ed53
hotire <gngh0101@gmail.com>
2021-08-18 01:14:41 +0900

    Add EntityScanConfig

---
+++ service/src/main/java/com/github/hotire/springcore/scan/EntityScanConfig.java
@@ -0,0 +1,7 @@
+package com.github.hotire.springcore.scan;
+
+/**
+ * @see org.springframework.boot.autoconfigure.domain.EntityScan
+ */
+public class EntityScanConfig {
+}
---
+++ service/src/main/java/com/github/hotire/springcore/scan/README.md
@@ -0,0 +1,5 @@
+# Scan
+
+## @EntityScan vs. @ComponentScan
+
+- https://www.baeldung.com/spring-entityscan-vs-componentscan
