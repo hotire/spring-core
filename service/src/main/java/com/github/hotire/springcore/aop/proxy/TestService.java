3ed6df43e78386cbcf88090250900486dd4b7bde
hotire <gngh0101@gmail.com>
2021-12-16 00:17:28 +0900

    Learn proxy in aop

---
+++ service/src/main/java/com/github/hotire/springcore/aop/proxy/TestService.java
@@ -0,0 +1,19 @@
+package com.github.hotire.springcore.aop.proxy;
+
+import org.springframework.context.ApplicationContext;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class TestService {
+    private final ApplicationContext context;
+
+    public void service() {
+        context.getBean(TestService.class).transaction();
+    }
+
+//    @Transactional
+    public void transaction() {
+        // ..
+    }
+}
---
+++ service/src/main/java/com/github/hotire/springcore/aop/proxy/TestServiceByStatic.java
@@ -0,0 +1,13 @@
+package com.github.hotire.springcore.aop.proxy;
+
+public class TestServiceByStatic {
+
+    public void service() {
+//        Application.getBean(TestServiceByStatic.class).transaction();;
+    }
+
+//    @Transactional
+    public void transaction() {
+        // ..
+    }
+}
