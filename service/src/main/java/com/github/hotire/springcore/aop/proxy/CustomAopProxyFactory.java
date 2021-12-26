c1e64c2211579bd152aa1732c2a667ad74b2dcba
hotire <gngh0101@gmail.com>
2021-12-26 17:05:31 +0900

    Add CustomAopProxyFactory

---
+++ service/src/main/java/com/github/hotire/springcore/aop/proxy/CustomAopProxyFactory.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springcore.aop.proxy;
+
+/**
+ * @see org.springframework.aop.framework.AopProxyFactory
+ * @see org.springframework.aop.framework.DefaultAopProxyFactory
+ */
+public class CustomAopProxyFactory {
+}
