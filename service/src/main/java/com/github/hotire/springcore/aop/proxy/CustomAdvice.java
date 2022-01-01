b515e9806db8d245f6de8dc9c62d4892fc834b88
hotire <gngh0101@gmail.com>
2022-01-01 14:28:49 +0900

    Add CustomAdvice

---
+++ service/src/main/java/com/github/hotire/springcore/aop/proxy/CustomAdvice.java
@@ -0,0 +1,18 @@
+package com.github.hotire.springcore.aop.proxy;
+
+import org.aopalliance.aop.Advice;
+import org.aopalliance.intercept.Interceptor;
+
+/**
+ * @see Advice
+ * @see
+ */
+public class CustomAdvice implements Advice {
+
+    /**
+     * @see org.aopalliance.intercept.Interceptor
+     */
+    public static class CustomInterceptor implements Interceptor {
+    }
+
+}
