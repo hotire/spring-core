51dc3ed747ac4df2809297e864e7f5478c9b7fa0
hotire <gngh0101@gmail.com>
2022-01-22 14:53:53 +0900

    Add Component annoatnion for MyConverter

--- service/src/main/java/com/github/hotire/springcore/converter/MyConverter.java
+++ service/src/main/java/com/github/hotire/springcore/converter/MyConverter.java
@@ -1,12 +1,18 @@
 package com.github.hotire.springcore.converter;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
 /**
  * @see Converter
  */
+@Component
 public class MyConverter implements Converter<MyConverterRequest, String> {
-    
+
+    public MyConverter() {
+        System.out.println();
+    }
+
     @Override
     public String convert(MyConverterRequest source) {
         return source.getName();
