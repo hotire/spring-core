f86aabe3552ecc8268bf7832094b4de4c8e0a95a
hotire <gngh0101@gmail.com>
2022-01-23 23:38:52 +0900

    Verify MyConverterTest

---
+++ service/src/main/java/com/github/hotire/springcore/converter/MyConverterController.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springcore.converter;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
+public class MyConverterController {
+}
---
+++ service/src/test/java/com/github/hotire/springcore/converter/MyConverterTest.java
@@ -0,0 +1,12 @@
+package com.github.hotire.springcore.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(MyConverterController.class)
+class MyConverterTest {
+
+    @Test
+    void di() {
+    }
+}
