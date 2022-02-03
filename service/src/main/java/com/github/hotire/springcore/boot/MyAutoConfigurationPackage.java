41482908bbd3ec9caed3c2bdd1d96045d97a2afe
hotire <gngh0101@gmail.com>
2022-02-04 00:51:35 +0900

    Add MyAutoConfigurationPackage

---
+++ service/src/main/java/com/github/hotire/springcore/boot/MyAutoConfigurationPackage.java
@@ -0,0 +1,18 @@
+package com.github.hotire.springcore.boot;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Import;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import(DefaultAutoConfigurationPackages.CustomRegister.class)
+public @interface MyAutoConfigurationPackage {
+}
