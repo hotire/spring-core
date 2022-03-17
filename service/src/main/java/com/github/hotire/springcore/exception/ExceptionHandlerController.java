908f9f15b3d25925b33587e1e6db5e5bf459e48c
hotire <gngh0101@gmail.com>
2022-03-17 09:57:24 +0900

    Implement AbstractErrorController

--- service/src/main/java/com/github/hotire/springcore/exception/ExceptionHandlerController.java
+++ service/src/main/java/com/github/hotire/springcore/exception/ExceptionHandlerController.java
@@ -1,15 +1,39 @@
 package com.github.hotire.springcore.exception;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RestController
 @RestControllerAdvice
-public class ExceptionHandlerController {
+public class ExceptionHandlerController extends AbstractErrorController {
+
+    public ExceptionHandlerController(ErrorAttributes errorAttributes) {
+        super(errorAttributes);
+    }
+
+    @RequestMapping("${server.error.path:${error.path:/error}}")
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        HttpStatus status = getStatus(request);
+        if (status == HttpStatus.NO_CONTENT) {
+            return new ResponseEntity<>(status);
+        }
+        Map<String, Object> body = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        return new ResponseEntity<>(body, status);
+    }
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<String> exception(CustomException e) {
