9421af11c7a65f92e31244f3374b123232708b00
hotire <gngh0101@gmail.com>
2022-04-16 13:01:46 +0900

    Verify factoryBean

--- service/src/main/java/com/github/hotire/springcore/bean/register/factory/ToolUser.java
+++ service/src/main/java/com/github/hotire/springcore/bean/register/factory/ToolUser.java
@@ -1,6 +1,10 @@
 package com.github.hotire.springcore.bean.register.factory;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.config.AbstractFactoryBean;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +13,11 @@ import lombok.RequiredArgsConstructor;
 public class ToolUser {
     private final Tool toolFactory;
     private final Tool toolFactory2;
+    private final AbstractFactoryBean<Tool> factoryBean;
+
+    @PostConstruct
+    public void init() throws Exception {
+        Assert.isTrue(factoryBean.getObject() == toolFactory2, "factory bean is diff");
+    }
+
 }
