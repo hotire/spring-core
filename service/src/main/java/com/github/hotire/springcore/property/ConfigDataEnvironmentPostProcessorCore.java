b9f81ade9aca623c623bc1ec3897d96ecd7b1f0c
hotire <gngh0101@gmail.com>
2022-04-17 14:11:54 +0900

    Learn ConfigDataEnvironmentPostProcessorCore

---
+++ service/src/main/java/com/github/hotire/springcore/property/ConfigDataEnvironmentPostProcessorCore.java
@@ -0,0 +1,37 @@
+package com.github.hotire.springcore.property;
+
+import java.util.Collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.config.ConfigDataEnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.io.ResourceLoader;
+
+/**
+ * @see ConfigDataEnvironmentPostProcessor
+ */
+public class ConfigDataEnvironmentPostProcessorCore {
+
+    /**
+     * @see ConfigDataEnvironmentPostProcessor#postProcessEnvironment(ConfigurableEnvironment, SpringApplication)
+     */
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        postProcessEnvironment(environment, application.getResourceLoader(), application.getAdditionalProfiles());
+    }
+
+    /**
+     * @see ConfigDataEnvironmentPostProcessor#postProcessEnvironment(ConfigurableEnvironment, ResourceLoader, Collection)
+     */
+    void postProcessEnvironment(ConfigurableEnvironment environment, ResourceLoader resourceLoader,
+                                Collection<String> additionalProfiles) {
+
+    }
+
+    /**
+     * @see ConfigDataEnvironmentPostProcessor#getConfigDataEnvironment(ConfigurableEnvironment, ResourceLoader, Collection) 
+     */
+    ConfigDataEnvironmentCore getConfigDataEnvironment(ConfigurableEnvironment environment, ResourceLoader resourceLoader,
+                                                   Collection<String> additionalProfiles) {
+        return null;
+    }
+}
