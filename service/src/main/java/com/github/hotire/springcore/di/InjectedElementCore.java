a2f4df6e87dc79030173554468dc63af38f74340
hotire <gngh0101@gmail.com>
2022-04-24 00:12:38 +0900

    Add InjectedElementCore

---
+++ service/src/main/java/com/github/hotire/springcore/di/InjectedElementCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springcore.di;
+
+/**
+ * @see org.springframework.beans.factory.annotation.InjectionMetadata.InjectedElement
+ * @see org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor.AutowiredFieldElement
+ * @see org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor.AutowiredMethodElement
+ */
+public class InjectedElementCore {
+}
