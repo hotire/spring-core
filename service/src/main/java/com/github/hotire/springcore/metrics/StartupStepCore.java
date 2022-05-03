3ce812a1ef999e6feb5489b7340d7cad53b09d8a
hotire <gngh0101@gmail.com>
2022-05-03 09:41:14 +0900

    Learn FlightRecorderStartup

---
+++ service/src/main/java/com/github/hotire/springcore/metrics/StartupStepCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springcore.metrics;
+
+/**
+ * @see org.springframework.core.metrics.StartupStep
+ * @see org.springframework.core.metrics.jfr.FlightRecorderStartupStep
+ * @see org.springframework.core.metrics.jfr.FlightRecorderStartupEvent
+ */
+public class StartupStepCore {
+}
