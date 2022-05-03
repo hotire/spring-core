e2b20cd1f64e29992f8356e96698fd8a7cc102a9
hotire <gngh0101@gmail.com>
2022-05-03 09:42:28 +0900

    Add ObjectFactoryCore

---
+++ service/src/main/java/com/github/hotire/springcore/bean/register/object_factory/ObjectFactoryCore.java
@@ -0,0 +1,17 @@
+package com.github.hotire.springcore.bean.register.object_factory;
+
+import org.springframework.beans.BeansException;
+
+/**
+ * @see org.springframework.beans.factory.ObjectFactory
+ * @see org.springframework.beans.factory.support.DefaultListableBeanFactory.DependencyObjectProvider
+ */
+public class ObjectFactoryCore {
+
+    /**
+     * @see org.springframework.beans.factory.support.DefaultListableBeanFactory.DependencyObjectProvider#getObject()
+     */
+    public Object getObject() throws BeansException {
+        return null;
+    }
+}
