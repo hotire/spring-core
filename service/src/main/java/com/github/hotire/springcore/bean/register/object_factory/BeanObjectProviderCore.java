aac45aa915dfeb50a055816c8e73e82533c88efc
hotire <gngh0101@gmail.com>
2022-05-07 13:32:12 +0900

    Add BeanObjectProviderCore

---
+++ service/src/main/java/com/github/hotire/springcore/bean/register/object_factory/BeanObjectProviderCore.java
@@ -0,0 +1,7 @@
+package com.github.hotire.springcore.bean.register.object_factory;
+
+/**
+ * @see org.springframework.beans.factory.support.DefaultListableBeanFactory.BeanObjectProvider
+ */
+public class BeanObjectProviderCore {
+}
