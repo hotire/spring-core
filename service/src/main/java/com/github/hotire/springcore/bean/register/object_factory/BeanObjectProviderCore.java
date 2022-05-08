a1be44ddaa848fb325f1aee124a3dd8baba5b814
hotire <gngh0101@gmail.com>
2022-05-08 16:25:25 +0900

    Implement ObjectProvider

--- service/src/main/java/com/github/hotire/springcore/bean/register/object_factory/BeanObjectProviderCore.java
+++ service/src/main/java/com/github/hotire/springcore/bean/register/object_factory/BeanObjectProviderCore.java
@@ -1,7 +1,9 @@
 package com.github.hotire.springcore.bean.register.object_factory;
 
+import org.springframework.beans.factory.ObjectProvider;
+
 /**
  * @see org.springframework.beans.factory.support.DefaultListableBeanFactory.BeanObjectProvider
  */
-public interface BeanObjectProviderCore<T> {
+public interface BeanObjectProviderCore<T> extends ObjectProvider<T> {
 }
--- service/src/main/java/com/github/hotire/springcore/bean/register/object_factory/DependencyObjectProviderCore.java
+++ service/src/main/java/com/github/hotire/springcore/bean/register/object_factory/DependencyObjectProviderCore.java
@@ -1,5 +1,11 @@
 package com.github.hotire.springcore.bean.register.object_factory;
 
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.TypeConverter;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.DependencyDescriptor;
 import org.springframework.lang.Nullable;
 
@@ -14,4 +20,42 @@ public class DependencyObjectProviderCore implements BeanObjectProviderCore<Obje
     private final boolean optional;
     @Nullable
     private final String beanName;
+
+    @Override
+    public Object getObject(Object... args) throws BeansException {
+        if (this.optional) {
+            return createOptionalDependency(this.descriptor, this.beanName);
+        } else {
+            Object result = doResolveDependency(this.descriptor, this.beanName, null, null);
+            if (result == null) {
+                throw new NoSuchBeanDefinitionException(this.descriptor.getResolvableType());
+            }
+            return result;
+        }
+    }
+
+    @Override
+    public Object getIfAvailable() throws BeansException {
+        return null;
+    }
+
+    @Override
+    public Object getIfUnique() throws BeansException {
+        return null;
+    }
+
+    @Override
+    public Object getObject() throws BeansException {
+        return null;
+    }
+
+    private Optional<?> createOptionalDependency(
+            DependencyDescriptor descriptor, @Nullable String beanName, final Object... args) {
+        return Optional.empty();
+    }
+
+    public Object doResolveDependency(DependencyDescriptor descriptor, @Nullable String beanName,
+                                      @Nullable Set<String> autowiredBeanNames, @Nullable TypeConverter typeConverter) throws BeansException {
+        return null;
+    }
 }
