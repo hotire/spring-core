7177b1850eeafdd259f8f858c76ffdc6ae89f749
hotire <gngh0101@gmail.com>
2022-05-15 01:03:29 +0900

    Learn finishRefresh

--- service/src/main/java/com/github/hotire/springcore/context/ApplicationContextCore.java
+++ service/src/main/java/com/github/hotire/springcore/context/ApplicationContextCore.java
@@ -4,7 +4,11 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.LifecycleProcessor;
 import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.DefaultLifecycleProcessor;
+
+import lombok.Getter;
 
 /**
  * @see ApplicationContext
@@ -13,11 +17,17 @@ import org.springframework.context.support.AbstractApplicationContext;
  */
 public class ApplicationContextCore {
 
+    @Getter
+    private final LifecycleProcessor lifecycleProcessor = new DefaultLifecycleProcessor();
+
     /**
-     * @see ConfigurableApplicationContext#refresh() 
-     * @see AbstractApplicationContext#refresh() 
+     * @see ConfigurableApplicationContext#refresh()
+     * @see AbstractApplicationContext#refresh()
      */
     public void refresh() throws BeansException, IllegalStateException {
+
+        // Last step: publish corresponding event.
+        finishRefresh();
 
     }
     /**
@@ -30,7 +40,14 @@ public class ApplicationContextCore {
      * @see AbstractApplicationContext#invokeBeanFactoryPostProcessors(ConfigurableListableBeanFactory)
      */
     protected void invokeBeanFactoryPostProcessors(ConfigurableListableBeanFactory beanFactory) {
-        
+
+    }
+
+    /**
+     * @see AbstractApplicationContext#finishRefresh()
+     */
+    protected void finishRefresh() {
+        getLifecycleProcessor().onRefresh();
     }
 
 }
