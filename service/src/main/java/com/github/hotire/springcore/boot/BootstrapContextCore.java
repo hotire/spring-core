74c427732e1db4e416af6f1bf682287a5e7a60fb
hotire <gngh0101@gmail.com>
2022-05-16 01:01:35 +0900

    Learn BootstrapContextCore

---
+++ service/src/main/java/com/github/hotire/springcore/boot/BootstrapContextCore.java
@@ -0,0 +1,13 @@
+package com.github.hotire.springcore.boot;
+
+/**
+ * @see  org.springframework.boot.BootstrapContext
+ * @see  org.springframework.boot.DefaultBootstrapContext
+ *
+ * 'ApplicationContext'가 준비되는 시점까지 시작 및 'Environment' 후처리 중에 사용할 수 있는 Context로
+ *
+ * 생성 비용이 많이 들거나 애플리케이션 컨텍스트를 사용할 수 있기 전에 공유해야 하는 싱글톤에 대한 지연 액세스를 제공합니다.
+ *
+ */
+public class BootstrapContextCore {
+}
