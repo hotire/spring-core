fa644d6ba4b1ea9e0cde5de3d31a69a8a7a54f20
hotire <gngh0101@gmail.com>
2022-05-17 01:10:27 +0900

    Add EnvironmentPostProcessorApplicationListenerCore

---
+++ service/src/main/java/com/github/hotire/springcore/property/EnvironmentPostProcessorApplicationListenerCore.java
@@ -0,0 +1,32 @@
+package com.github.hotire.springcore.property;
+
+import org.springframework.boot.ConfigurableBootstrapContext;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.boot.context.event.ApplicationFailedEvent;
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
+import org.springframework.boot.env.EnvironmentPostProcessorApplicationListener;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * @see EnvironmentPostProcessorApplicationListener
+ */
+public class EnvironmentPostProcessorApplicationListenerCore {
+
+    /**
+     * @see org.springframework.boot.SpringApplicationRunListeners#environmentPrepared(ConfigurableBootstrapContext, ConfigurableEnvironment)
+     * @see  org.springframework.boot.context.event.EventPublishingRunListener#environmentPrepared(ConfigurableBootstrapContext, ConfigurableEnvironment) 
+     * @see EnvironmentPostProcessorApplicationListener#onApplicationEvent(ApplicationEvent)
+     */
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof ApplicationEnvironmentPreparedEvent) {
+//            onApplicationEnvironmentPreparedEvent((ApplicationEnvironmentPreparedEvent) event);
+        }
+        if (event instanceof ApplicationPreparedEvent) {
+//            onApplicationPreparedEvent();
+        }
+        if (event instanceof ApplicationFailedEvent) {
+//            onApplicationFailedEvent();
+        }
+    }
+}
