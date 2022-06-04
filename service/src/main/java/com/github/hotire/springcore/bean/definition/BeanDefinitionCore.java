fb3211aa07fa525a9f2989e740f7ee5a143ba417
hotire <gngh0101@gmail.com>
2022-06-04 18:16:51 +0900

    Add BeanDefinitionCore

---
+++ service/src/main/java/com/github/hotire/springcore/bean/definition/BeanDefinitionCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springcore.bean.definition;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+
+/**
+ * @see BeanDefinition
+ */
+public class BeanDefinitionCore {
+}
---
+++ service/src/main/java/com/github/hotire/springcore/bean/definition/BeanDefinitionRegistryCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springcore.bean.definition;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+
+/**
+ * @see BeanDefinitionRegistry
+ */
+public class BeanDefinitionRegistryCore {
+}
