99aeb156eb322ac9faed7c05c57dac925a4a4713
hotire <gngh0101@gmail.com>
2022-06-05 00:34:41 +0900

    Learn EnvironmentPostProcessorsFactoryCore

---
+++ service/src/main/java/com/github/hotire/springcore/env/EnvironmentPostProcessorsFactoryCore.java
@@ -0,0 +1,16 @@
+package com.github.hotire.springcore.env;
+
+import org.springframework.boot.env.EnvironmentPostProcessorsFactory;
+
+/**
+ * @see EnvironmentPostProcessorsFactory
+ */
+public class EnvironmentPostProcessorsFactoryCore {
+
+    /**
+     * @see EnvironmentPostProcessorsFactory#fromSpringFactories(ClassLoader)
+     */
+    static EnvironmentPostProcessorsFactory fromSpringFactories(ClassLoader classLoader) {
+        return null;
+    }
+}
