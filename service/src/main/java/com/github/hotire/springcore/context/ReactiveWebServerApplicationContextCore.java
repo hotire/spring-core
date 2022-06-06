f20e75e478362d9038f59a9aade13f1bf5a0d8b2
hotire <gngh0101@gmail.com>
2022-06-07 00:45:56 +0900

    Update ApplicationContextCore

---
+++ service/src/main/java/com/github/hotire/springcore/context/ReactiveWebServerApplicationContextCore.java
@@ -0,0 +1,7 @@
+package com.github.hotire.springcore.context;
+
+/**
+ * @see org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext
+ */
+public class ReactiveWebServerApplicationContextCore {
+}
