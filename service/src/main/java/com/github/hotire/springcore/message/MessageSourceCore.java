43b82974f7f80cf9a969e239517def97866b3e87
hotire <gngh0101@gmail.com>
2022-06-08 00:04:29 +0900

    Add MessageSourceCore

---
+++ service/src/main/java/com/github/hotire/springcore/message/MessageSourceCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springcore.message;
+
+/**
+ * @see org.springframework.context.MessageSource
+ * @see org.springframework.context.support.DelegatingMessageSource
+ */
+public class MessageSourceCore {
+}
