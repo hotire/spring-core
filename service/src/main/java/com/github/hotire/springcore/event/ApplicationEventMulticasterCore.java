a7dc634a97f1dba732b9751fd0584867e415f213
hotire <gngh0101@gmail.com>
2022-06-08 00:08:00 +0900

    Add ApplicationEventMulticasterCore

---
+++ service/src/main/java/com/github/hotire/springcore/event/ApplicationEventMulticasterCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springcore.event;
+
+/**
+ * @see org.springframework.context.event.ApplicationEventMulticaster
+ * @see org.springframework.context.event.AbstractApplicationEventMulticaster
+ * @see org.springframework.context.event.SimpleApplicationEventMulticaster
+ */
+public class ApplicationEventMulticasterCore {
+}
