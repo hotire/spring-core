c3851ec084df3d8cc9961a4e0c81cd91654f4a71
hotire <gngh0101@gmail.com>
2022-06-12 01:31:40 +0900

    Update EnvironmentPostProcessorsFactoryCore

--- service/src/main/java/com/github/hotire/springcore/env/EnvironmentPostProcessorsFactoryCore.java
+++ service/src/main/java/com/github/hotire/springcore/env/EnvironmentPostProcessorsFactoryCore.java
@@ -9,6 +9,7 @@ public class EnvironmentPostProcessorsFactoryCore {
 
     /**
      * @see EnvironmentPostProcessorsFactory#fromSpringFactories(ClassLoader)
+     * EnvironmentPostProcessorApplicationListener 생성시 해당 메서스를 통해 EnvironmentPostProcessor 구현체를 load 한다.
      */
     static EnvironmentPostProcessorsFactory fromSpringFactories(ClassLoader classLoader) {
         return null;
