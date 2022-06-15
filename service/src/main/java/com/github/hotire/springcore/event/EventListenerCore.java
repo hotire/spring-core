013601775e630e62f9a5433796faeb36b594135f
hotire <gngh0101@gmail.com>
2022-06-15 09:37:14 +0900

    Add EventListenerCore

---
+++ service/src/main/java/com/github/hotire/springcore/event/EventListenerCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.event;
+
+/**
+ * @see org.springframework.context.event.EventListener
+ * @see org.springframework.context.event.EventListenerMethodProcessor
+ * @see org.springframework.context.event.DefaultEventListenerFactory
+ * @see org.springframework.context.event.ApplicationListenerMethodAdapter
+ */
+public class EventListenerCore {
+}
