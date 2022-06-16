7d670e0b80cbc0a121d203b5d708f5c5ed2601b3
hotire <gngh0101@gmail.com>
2022-06-17 00:38:48 +0900

    Add AopProxyCore

---
+++ service/src/main/java/com/github/hotire/springcore/aop/AopProxyCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springcore.aop;
+
+/**
+ * @see org.springframework.aop.framework.AopProxy
+ * @see org.springframework.aop.framework.AopProxyFactory
+ */
+public class AopProxyCore {
+}
