1f85aba49ead993ddcf100300328b4c49d0b8b23
hotire <gngh0101@gmail.com>
2022-06-17 00:37:59 +0900

    Add AdvisorCore

---
+++ service/src/main/java/com/github/hotire/springcore/aop/AdvisorCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springcore.aop;
+
+/**
+ * @see org.springframework.aop.Advisor
+ * @see org.springframework.aop.aspectj.annotation.InstantiationModelAwarePointcutAdvisorImpl
+ */
+public class AdvisorCore {
+}
