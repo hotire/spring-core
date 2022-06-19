92cc6e85723273a52f3d7a94b1d88b99c5e107f9
hotire <gngh0101@gmail.com>
2022-06-20 00:06:09 +0900

    Add AutowiredAnnotationBeanPostProcessor

---
+++ service/src/main/java/com/github/hotire/springcore/di/AutowiredAnnotationBeanPostProcessorCore.java
@@ -0,0 +1,7 @@
+package com.github.hotire.springcore.di;
+
+/**
+ * @see org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor
+ */
+public class AutowiredAnnotationBeanPostProcessorCore {
+}
