9fe35b933c532cf5f0748305905226214c08402b
hotire <gngh0101@gmail.com>
2022-06-20 00:08:46 +0900

    Learn postProcessProperties

--- service/src/main/java/com/github/hotire/springcore/di/AutowiredAnnotationBeanPostProcessorCore.java
+++ service/src/main/java/com/github/hotire/springcore/di/AutowiredAnnotationBeanPostProcessorCore.java
@@ -1,7 +1,20 @@
 package com.github.hotire.springcore.di;
 
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.PropertyValues;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+
 /**
  * @see org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor
  */
 public class AutowiredAnnotationBeanPostProcessorCore {
+
+    /**
+     * DI field, setter method
+     * @see org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory#populateBean(String, RootBeanDefinition, BeanWrapper)
+     * @see org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor#postProcessProperties(PropertyValues, Object, String)
+     */
+    public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) {
+        return null;
+    }
 }
