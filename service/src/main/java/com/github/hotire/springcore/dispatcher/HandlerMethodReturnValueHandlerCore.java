fbb03c39fa9cf093011253d83b419607b3a4c2ed
hotire <gngh0101@gmail.com>
2022-06-21 00:02:09 +0900

    Add HandlerMethodReturnValueHandlerCore

---
+++ service/src/main/java/com/github/hotire/springcore/dispatcher/HandlerMethodReturnValueHandlerCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springcore.dispatcher;
+
+/**
+ * @see org.springframework.web.method.support.HandlerMethodReturnValueHandler
+ * @see org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor
+ */
+public class HandlerMethodReturnValueHandlerCore {
+}
