1690d8bf30a12b4e7ce7e406de0e8066e77c6f5d
hotire <gngh0101@gmail.com>
2022-06-26 16:13:01 +0900

    Update CustomFilterConfig

--- service/src/main/java/com/github/hotire/springcore/filter/CustomFilterConfig.java
+++ service/src/main/java/com/github/hotire/springcore/filter/CustomFilterConfig.java
@@ -9,26 +9,49 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 public class CustomFilterConfig {
 
+    public static class FilterService {
+
+    }
+
     public static class LoggingFilter implements Filter {
+
+        @Autowired
+        private FilterService filterService;
+
         @Override
         public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+            log.info("LoggingFilter");
             filterChain.doFilter(servletRequest, servletResponse);
         }
     }
 
     @Bean
-    public FilterRegistrationBean<LoggingFilter> loggingFilter() {
+    public FilterRegistrationBean<LoggingFilter> filterRegistrationBean() {
         final FilterRegistrationBean<LoggingFilter> bean = new FilterRegistrationBean<>();
-        bean.setFilter(new LoggingFilter());
+        bean.setFilter(loggingFilter());
         bean.setUrlPatterns(List.of("*"));
         return bean;
+    }
+
+    @Bean
+    public LoggingFilter loggingFilter() {
+        return new LoggingFilter();
+    }
+
+    @Bean
+    public FilterService filterService() {
+        return new FilterService();
     }
 
 }
