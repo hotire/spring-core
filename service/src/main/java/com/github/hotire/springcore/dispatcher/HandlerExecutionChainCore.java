8fcbff7cee1bfb351bae375040bc5c6ad3466df5
hotire <gngh0101@gmail.com>
2022-06-26 16:38:18 +0900

    Learn HandlerMappingCore

---
+++ service/src/main/java/com/github/hotire/springcore/dispatcher/HandlerExecutionChainCore.java
@@ -0,0 +1,19 @@
+package com.github.hotire.springcore.dispatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerExecutionChain;
+
+/**
+ * @see HandlerExecutionChain
+ */
+public class HandlerExecutionChainCore {
+
+    /**
+     * @see HandlerExecutionChain#applyPreHandle(HttpServletRequest, HttpServletResponse) 
+     */
+    boolean applyPreHandle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return false;
+    }
+}
---
+++ service/src/main/java/com/github/hotire/springcore/dispatcher/HandlerMappingCore.java
@@ -0,0 +1,19 @@
+package com.github.hotire.springcore.dispatcher;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.servlet.HandlerExecutionChain;
+
+/**
+ * @see org.springframework.web.servlet.HandlerMapping
+ * @see org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping 
+ */
+public class HandlerMappingCore {
+
+    /**
+     * @see org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping#getHandler(HttpServletRequest) 
+     */
+    public HandlerExecutionChain getHandler(HttpServletRequest request) throws Exception {
+        return new HandlerExecutionChain(null);
+    }
+}
