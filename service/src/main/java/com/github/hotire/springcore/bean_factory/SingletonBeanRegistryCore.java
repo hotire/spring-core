55a84713c4ff4dffbd53c8b970202e7dbf8f32a1
hotire <gngh0101@gmail.com>
2022-07-06 00:01:33 +0900

    Add SingletonBeanRegistryCore

---
+++ service/src/main/java/com/github/hotire/springcore/bean_factory/SingletonBeanRegistryCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springcore.bean_factory;
+
+/**
+ * @see org.springframework.beans.factory.config.SingletonBeanRegistry
+ * @see org.springframework.beans.factory.support.DefaultSingletonBeanRegistry
+ */
+public class SingletonBeanRegistryCore {
+}
