f18525b0f2ca943b0fea53c735b9d737c6fd3e21
hotire <gngh0101@gmail.com>
2022-07-07 00:03:30 +0900

    Learn /interceptor

---
+++ service/src/main/java/com/github/hotire/springcore/interceptor/HandlerInterceptorCore.java
@@ -0,0 +1,7 @@
+package com.github.hotire.springcore.interceptor;
+
+/**
+ * @see org.springframework.web.servlet.HandlerInterceptor
+ */
+public class HandlerInterceptorCore {
+}
---
+++ service/src/main/java/com/github/hotire/springcore/interceptor/InterceptorRegistryCore.java
@@ -0,0 +1,7 @@
+package com.github.hotire.springcore.interceptor;
+
+/**
+ * @see org.springframework.web.servlet.config.annotation.InterceptorRegistry
+ */
+public class InterceptorRegistryCore {
+}
