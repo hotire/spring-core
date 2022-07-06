6c0bbc3c06e37d69ca53028cce6fb8a61a53e12b
hotire <gngh0101@gmail.com>
2022-07-07 00:03:50 +0900

    Add AspectCore

---
+++ service/src/main/java/com/github/hotire/springcore/aop/AspectCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springcore.aop;
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * @see Aspect
+ * @see org.springframework.aop.aspectj.annotation.AbstractAspectJAdvisorFactory
+ */
+public class AspectCore {
+}
