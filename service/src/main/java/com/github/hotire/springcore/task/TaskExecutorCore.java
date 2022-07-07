b45495df29e94bf94d0f2c587f60bd0f5faa83b5
hotire <gngh0101@gmail.com>
2022-07-08 00:02:58 +0900

    Add TaskExecutorCore

---
+++ service/src/main/java/com/github/hotire/springcore/task/TaskExecutorCore.java
@@ -0,0 +1,19 @@
+package com.github.hotire.springcore.task;
+
+import org.springframework.scheduling.concurrent.ExecutorConfigurationSupport;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * @see org.springframework.core.task.TaskExecutor
+ * @see ExecutorConfigurationSupport
+ * @see ThreadPoolTaskExecutor
+ */
+public class TaskExecutorCore {
+
+    /**
+     * @see ExecutorConfigurationSupport#afterPropertiesSet()
+     */
+    void afterPropertiesSet() {
+
+    }
+}
