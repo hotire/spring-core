3fffe68764e7cf5200263f39f3c1423c9bccfd75
hotire <gngh0101@gmail.com>
2022-07-09 00:54:24 +0900

    Update ProtocolHandlerCore

--- service/src/main/java/com/github/hotire/springcore/tomcat/ProtocolHandlerCore.java
+++ service/src/main/java/com/github/hotire/springcore/tomcat/ProtocolHandlerCore.java
@@ -1,5 +1,9 @@
 package com.github.hotire.springcore.tomcat;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.coyote.ProtocolHandler;
+
 /**
  * @see org.apache.coyote.ProtocolHandler
  * @see org.apache.coyote.AbstractProtocol
@@ -7,9 +11,16 @@ package com.github.hotire.springcore.tomcat;
 public class ProtocolHandlerCore {
 
     /**
-     * @see org.apache.coyote.AbstractProtocol#setMaxThreads(int) 
+     * @see org.apache.coyote.AbstractProtocol#setMaxThreads(int)
      */
     public void setMaxThreads(int maxThreads) {
-        
+
+    }
+
+    /**
+     * @see org.apache.coyote.ProtocolHandler#create(String, boolean)
+     */
+    public static ProtocolHandler create(String protocol, boolean apr) throws ClassNotFoundException, InvocationTargetException, InstantiationException, NoSuchMethodException, IllegalAccessException {
+        return ProtocolHandler.create(protocol, apr);
     }
 }
