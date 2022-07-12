9763e3e84fc886c19232d2fbb478f1ddb5d6c3cc
hotire <gngh0101@gmail.com>
2022-07-12 20:16:05 +0900

    Add ResourceHandlerRegistryCore

---
+++ service/src/main/java/com/github/hotire/springcore/resource/ResourceHandlerRegistryCore.java
@@ -0,0 +1,17 @@
+package com.github.hotire.springcore.resource;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+/**
+ * @see ResourceHandlerRegistry
+ */
+public class ResourceHandlerRegistryCore {
+
+    /**
+     * @see ResourceHandlerRegistry#addResourceHandler(String...) 
+     */
+    public ResourceHandlerRegistration addResourceHandler(String... pathPatterns) {
+        return null;
+    }
+}
