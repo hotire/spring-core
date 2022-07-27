65c19a2ed25fd1fb92e349181acd76feca35f664
hotire <gngh0101@gmail.com>
2022-07-27 14:33:37 +0900

    ServletWebServerFactoryAutoConfigurationCore

---
+++ service/src/main/java/com/github/hotire/springcore/server/ServletWebServerFactoryAutoConfigurationCore.java
@@ -0,0 +1,7 @@
+package com.github.hotire.springcore.server;
+
+/**
+ * @see org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration
+ */
+public class ServletWebServerFactoryAutoConfigurationCore {
+}
