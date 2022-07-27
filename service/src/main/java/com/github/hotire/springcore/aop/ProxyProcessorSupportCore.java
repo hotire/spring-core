6f91d3f8b3254afc521add907de120648047ea37
hotire <gngh0101@gmail.com>
2022-07-27 14:33:52 +0900

    Add ProxyProcessorSupportCore

---
+++ service/src/main/java/com/github/hotire/springcore/aop/ProxyProcessorSupportCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springcore.aop;
+
+/**
+ * @see org.springframework.aop.framework.ProxyProcessorSupport
+ * @see org.springframework.aop.framework.AbstractAdvisingBeanPostProcessor
+ * @see org.springframework.aop.framework.autoproxy.AbstractBeanFactoryAwareAdvisingPostProcessor
+ */
+public class ProxyProcessorSupportCore {
+}
