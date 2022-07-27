498e9a48a396d3d87cf6971fb1d3f7ad15d5a616
hotire <gngh0101@gmail.com>
2022-07-27 14:33:17 +0900

    Add WebServerGracefulShutdownLifecycleCore

---
+++ service/src/main/java/com/github/hotire/springcore/lfie_cycle/WebServerGracefulShutdownLifecycleCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springcore.lfie_cycle;
+
+/**
+ * since 2.5.0
+ * @see org.springframework.boot.web.context.WebServerGracefulShutdownLifecycle
+ */
+public class WebServerGracefulShutdownLifecycleCore {
+}
