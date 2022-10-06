995e5a4eec9f74d3983067dba2c52239864d1f40
hotire <gngh0101@gmail.com>
2022-10-07 01:55:20 +0900

    Upgrade 2.7.3 boot version

--- service/src/main/java/com/github/hotire/springcore/boot/auto/AutoConfigurationCore.java
+++ service/src/main/java/com/github/hotire/springcore/boot/auto/AutoConfigurationCore.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -15,17 +16,20 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * EnableAutoConfiguration has AutoConfigurationPackage annoation
  *
- * @Import(AutoConfigurationPackages.Registrar.class)
+ * @Import(AutoConfigura`tionPackages.Registrar.class)
  *
  * @see org.springframework.boot.autoconfigure.SpringBootApplication
  * @see EnableAutoConfiguration
  * @see AutoConfigurationPackage
  * @see org.springframework.boot.autoconfigure.AutoConfigurationPackages.Registrar is ImportBeanDefinitionRegistrar
+ *
+ * 2.7.0 after
+ * @see AutoConfiguration
  */
 @Slf4j
 @Configuration
 @MyAutoConfigurationPackage
-public class AutoConfiguration implements BeanFactoryAware {
+public class AutoConfigurationCore implements BeanFactoryAware {
 
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
