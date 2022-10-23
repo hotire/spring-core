547948ce23429337ab75b6046b91aca310c34142
hotire <gngh0101@gmail.com>
2022-10-23 14:33:23 +0900

    Learn getLogger

--- service/src/main/java/com/github/hotire/springcore/log/slf4j/LoggerFactoryCore.java
+++ service/src/main/java/com/github/hotire/springcore/log/slf4j/LoggerFactoryCore.java
@@ -1,5 +1,6 @@
 package com.github.hotire.springcore.log.slf4j;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -7,4 +8,10 @@ import org.slf4j.LoggerFactory;
  */
 public class LoggerFactoryCore {
 
+    /**
+     * @see LoggerFactory#getLogger(String)
+     */
+    public static Logger getLogger(String name) {
+        return LoggerFactory#getLogger(name);
+    }
 }
