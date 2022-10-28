eb0b74fa0e05fb24434e0108de7b58baa5ae592c
hotire <gngh0101@gmail.com>
2022-10-29 00:33:55 +0900

    Learn LoggerContext#getLogger(String)

--- service/src/main/java/com/github/hotire/springcore/log/slf4j/ILoggerFactoryCore.java
+++ service/src/main/java/com/github/hotire/springcore/log/slf4j/ILoggerFactoryCore.java
@@ -2,15 +2,18 @@ package com.github.hotire.springcore.log.slf4j;
 
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
+import ch.qos.logback.classic.LoggerContext;
 
 /**
  * @see ILoggerFactory
+ * @see LoggerContext
  */
 public interface ILoggerFactoryCore {
 
 
     /**
      * @see ILoggerFactory#getLogger(String)
+     * @see LoggerContext#getLogger(String) 
      */
      Logger getLogger(String name);
 }
