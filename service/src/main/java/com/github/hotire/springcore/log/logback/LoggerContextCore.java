e7a6c57dc2de7d652e0d5cd0aa75cedf3dc36621
hotire <gngh0101@gmail.com>
2022-10-30 19:56:43 +0900

    Add loggerCache

--- service/src/main/java/com/github/hotire/springcore/log/logback/LoggerContextCore.java
+++ service/src/main/java/com/github/hotire/springcore/log/logback/LoggerContextCore.java
@@ -1,9 +1,13 @@
 package com.github.hotire.springcore.log.logback;
+import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
+import java.util.Map;
 
 /**
  * @see LoggerContext
  */
 public class LoggerContextCore {
+
+    private Map<String, Logger> loggerCache;
 
 }
