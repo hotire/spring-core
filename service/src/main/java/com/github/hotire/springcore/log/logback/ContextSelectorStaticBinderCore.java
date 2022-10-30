88c47b9b893038d2d5eca72490de45ed8510cb0b
hotire <gngh0101@gmail.com>
2022-10-31 00:12:33 +0900

    Add ContextSelectorStaticBinder

---
+++ service/src/main/java/com/github/hotire/springcore/log/logback/ContextSelectorStaticBinderCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.log.logback;
+
+import ch.qos.logback.classic.util.ContextSelectorStaticBinder;
+
+/**
+ * @see ContextSelectorStaticBinder
+ */
+public class ContextSelectorStaticBinderCore {
+
+}
