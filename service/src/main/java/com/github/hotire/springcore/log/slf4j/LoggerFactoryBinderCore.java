a99b78809998a692eae4160000a4d1a468a06acf
hotire <gngh0101@gmail.com>
2022-10-31 00:10:17 +0900

    Add StaticLoggerBinder

--- service/src/main/java/com/github/hotire/springcore/log/slf4j/LoggerFactoryBinderCore.java
+++ service/src/main/java/com/github/hotire/springcore/log/slf4j/LoggerFactoryBinderCore.java
@@ -1,7 +1,10 @@
 package com.github.hotire.springcore.log.slf4j;
 import org.slf4j.spi.LoggerFactoryBinder;
+import org.slf4j.impl.StaticLoggerBinder;
+
 /**
  * @see LoggerFactoryBinder
+ * @see StaticLoggerBinder
  */
 public class LoggerFactoryBinderCore {
 
