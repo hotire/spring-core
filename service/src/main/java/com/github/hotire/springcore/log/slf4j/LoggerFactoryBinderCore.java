8ac3db7f007a1f94edfa4cb2bae4731da4ff28b9
hotire <gngh0101@gmail.com>
2022-10-31 00:11:27 +0900

    Update  LoggerFactoryBinder#getLoggerFactory()

--- service/src/main/java/com/github/hotire/springcore/log/slf4j/LoggerFactoryBinderCore.java
+++ service/src/main/java/com/github/hotire/springcore/log/slf4j/LoggerFactoryBinderCore.java
@@ -1,4 +1,5 @@
 package com.github.hotire.springcore.log.slf4j;
+import org.slf4j.ILoggerFactory;
 import org.slf4j.spi.LoggerFactoryBinder;
 import org.slf4j.impl.StaticLoggerBinder;
 
@@ -7,5 +8,13 @@ import org.slf4j.impl.StaticLoggerBinder;
  * @see StaticLoggerBinder
  */
 public class LoggerFactoryBinderCore {
+
+    /**
+     * @see LoggerFactoryBinder#getLoggerFactory()
+     * @see StaticLoggerBinder#getLoggerFactory()
+     */
+    public ILoggerFactory getLoggerFactory() {
+        return StaticLoggerBinder.getSingleton().getLoggerFactory();
+    }
 
 }
