76caea30447ba396d7db13537e06915ef978f515
hotire <gngh0101@gmail.com>
2022-10-31 00:15:27 +0900

    Learn init

--- service/src/main/java/com/github/hotire/springcore/log/logback/ContextSelectorStaticBinderCore.java
+++ service/src/main/java/com/github/hotire/springcore/log/logback/ContextSelectorStaticBinderCore.java
@@ -1,10 +1,25 @@
 package com.github.hotire.springcore.log.logback;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.selector.ContextSelector;
 import ch.qos.logback.classic.util.ContextSelectorStaticBinder;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * @see ContextSelectorStaticBinder
  */
 public class ContextSelectorStaticBinderCore {
+
+    /**
+     * @see ContextSelectorStaticBinder#contextSelector
+     */
+    ContextSelector contextSelector;
+
+    /**
+     * @see ContextSelectorStaticBinder#init(LoggerContext, Object)
+     */
+    public void init(LoggerContext defaultLoggerContext, Object key) throws ClassNotFoundException, NoSuchMethodException, InstantiationException,
+        IllegalAccessException, InvocationTargetException {
+    }
 
 }
