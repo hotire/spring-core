3c29112f4534001d343b8f6dcbeb4262fc2cd716
hotire <gngh0101@gmail.com>
2023-03-07 03:11:26 +0900

    Verify parameterType

--- service/src/test/java/com/github/hotire/springcore/bean/inner/InnerClassConfigTest.java
+++ service/src/test/java/com/github/hotire/springcore/bean/inner/InnerClassConfigTest.java
@@ -1,5 +1,7 @@
 package com.github.hotire.springcore.bean.inner;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import com.github.hotire.springcore.bean.inner.InnerClassConfig.Inner;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -9,8 +11,17 @@ class InnerClassConfigTest {
 
     @Test
     void create() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        // given
         final InnerClassConfig config = new InnerClassConfig();
         final Constructor<?>[] constructors = Inner.class.getDeclaredConstructors();
-        constructors[0].newInstance(config);
+        final Constructor<?> constructor = constructors[0];
+
+        // when
+        final Class<?> parameterType = constructor.getParameters()[0].getType();
+        final Object result = constructor.newInstance(config);
+
+        // then
+        assertThat(result).isInstanceOf(Inner.class);
+        assertThat(parameterType).isEqualTo(InnerClassConfig.class);
     }
 }
