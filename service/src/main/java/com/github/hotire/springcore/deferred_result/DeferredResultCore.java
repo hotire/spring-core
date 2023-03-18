43994b5902a7698da8a7f7e61dcfc01cd26f9233
hotire <gngh0101@gmail.com>
2023-03-18 11:57:12 +0900

    Add DeferredResultCore

---
+++ service/src/main/java/com/github/hotire/springcore/deferred_result/DeferredResultCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.deferred_result;
+
+import org.springframework.web.context.request.async.DeferredResult;
+
+/**
+ * @see DeferredResult
+ */
+public class DeferredResultCore {
+
+}
