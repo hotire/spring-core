06479f63a7c1308cd11e7441a4841b2e169555dc
hotire <gngh0101@gmail.com>
2023-03-19 11:30:33 +0900

    createId

--- service/src/main/java/com/github/hotire/springcore/deferred_result/DeferredResultContainer.java
+++ service/src/main/java/com/github/hotire/springcore/deferred_result/DeferredResultContainer.java
@@ -7,11 +7,27 @@ import org.springframework.web.context.request.async.DeferredResult;
 
 @RequiredArgsConstructor
 public class DeferredResultContainer {
-    private final Map<DeferredResultId, DeferredResult<?>> resultCache = new ConcurrentHashMap<>();
+    private final Map<DeferredResultId, DeferredResult<Object>> resultCache = new ConcurrentHashMap<>();
 
     @RequiredArgsConstructor
     static class DeferredResultId {
         private final String value;
+    }
+
+    public DeferredResultId createId(String value) {
+        return new DeferredResultId(value);
+    }
+
+    public <T> DeferredResult<T> of(final DeferredResultId id) {
+        final DeferredResult<T> deferredResult = new DeferredResult<>();
+        return deferredResult;
+    }
+
+    public <T> void publish(final DeferredResultId id,  final T result) {
+        resultCache.computeIfPresent(id, (deferredResultId, objectDeferredResult) -> {
+            objectDeferredResult.setResult(result);
+            return null;
+        });
     }
 
 }
