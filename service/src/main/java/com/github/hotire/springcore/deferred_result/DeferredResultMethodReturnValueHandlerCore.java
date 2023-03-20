0075ce75ce0544b5371af305254e4fbb98cfe2b9
hotire <gngh0101@gmail.com>
2023-03-21 03:17:16 +0900

    Add DeferredResultMethodReturnValueHandlerCore

---
+++ service/src/main/java/com/github/hotire/springcore/deferred_result/DeferredResultMethodReturnValueHandlerCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.deferred_result;
+
+import org.springframework.web.servlet.mvc.method.annotation.DeferredResultMethodReturnValueHandler;
+
+/**
+ * @see DeferredResultMethodReturnValueHandler
+ */
+public class DeferredResultMethodReturnValueHandlerCore {
+
+}
