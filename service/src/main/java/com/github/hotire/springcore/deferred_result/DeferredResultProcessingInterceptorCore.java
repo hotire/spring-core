3fda16329c3a43e8e555ecea29d2c1e474dae018
hotire <gngh0101@gmail.com>
2023-03-24 02:21:18 +0900

    DeferredResultProcessingInterceptorCore

---
+++ service/src/main/java/com/github/hotire/springcore/deferred_result/DeferredResultProcessingInterceptorCore.java
@@ -0,0 +1,12 @@
+package com.github.hotire.springcore.deferred_result;
+
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.context.request.async.DeferredResultProcessingInterceptor;
+
+/**
+ * @see DeferredResultProcessingInterceptor
+ * @see DeferredResult#getInterceptor()
+ */
+public class DeferredResultProcessingInterceptorCore {
+
+}
