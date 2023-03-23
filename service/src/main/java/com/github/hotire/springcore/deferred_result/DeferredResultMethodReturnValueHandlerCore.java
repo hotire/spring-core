7bc1eba571abf6ff8feb669e616bac099a362988
hotire <gngh0101@gmail.com>
2023-03-24 02:21:55 +0900

    handleReturnValue in DeferredResultMethodReturnValueHandler

--- service/src/main/java/com/github/hotire/springcore/deferred_result/DeferredResultMethodReturnValueHandlerCore.java
+++ service/src/main/java/com/github/hotire/springcore/deferred_result/DeferredResultMethodReturnValueHandlerCore.java
@@ -1,5 +1,9 @@
 package com.github.hotire.springcore.deferred_result;
 
+import org.springframework.core.MethodParameter;
+import org.springframework.lang.Nullable;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.mvc.method.annotation.DeferredResultMethodReturnValueHandler;
 
 /**
@@ -7,4 +11,11 @@ import org.springframework.web.servlet.mvc.method.annotation.DeferredResultMetho
  */
 public class DeferredResultMethodReturnValueHandlerCore {
 
+    /**
+     * @see DeferredResultMethodReturnValueHandler#handleReturnValue(Object, MethodParameter, ModelAndViewContainer, NativeWebRequest) 
+     */
+    public void handleReturnValue(@Nullable Object returnValue, MethodParameter returnType,
+        ModelAndViewContainer mavContainer, NativeWebRequest webRequest) throws Exception {
+        
+    }
 }
