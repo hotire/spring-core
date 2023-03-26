8d6d12b17dd0df322d0b468f8b47604cbfb4b4d9
hotire <gngh0101@gmail.com>
2023-03-27 00:18:44 +0900

    startCallableProcessing

--- service/src/main/java/com/github/hotire/springcore/async/WebAsyncManagerCore.java
+++ service/src/main/java/com/github/hotire/springcore/async/WebAsyncManagerCore.java
@@ -1,10 +1,18 @@
 package com.github.hotire.springcore.async;
 
+import java.util.concurrent.Callable;
 import org.springframework.web.context.request.async.WebAsyncManager;
 
 /**
  * @see WebAsyncManager
  */
 public class WebAsyncManagerCore {
+
+    /**
+     * @see WebAsyncManager#startCallableProcessing(Callable, Object...) 
+     */
+    public void startCallableProcessing(Callable<?> callable, Object... processingContext) throws Exception {
+        
+    }
 
 }
