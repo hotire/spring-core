aa107354147fb85f9f0e6fe7f2ec6c939b0ef79d
hotire <gngh0101@gmail.com>
2023-03-29 01:16:57 +0900

    Add AsyncWebRequestCore

---
+++ service/src/main/java/com/github/hotire/springcore/async/AsyncWebRequestCore.java
@@ -0,0 +1,12 @@
+package com.github.hotire.springcore.async;
+
+import org.springframework.web.context.request.async.AsyncWebRequest;
+import org.springframework.web.context.request.async.StandardServletAsyncWebRequest;
+
+/**
+ * @see AsyncWebRequest
+ * @see StandardServletAsyncWebRequest
+ */
+public class AsyncWebRequestCore {
+
+}
