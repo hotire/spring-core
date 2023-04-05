57522ef1c068e0aef742bebba05fc7e1bd02422c
hotire <gngh0101@gmail.com>
2023-04-06 02:27:38 +0900

    conversionService

--- service/src/test/java/com/github/hotire/springcore/test/MockMvcBuildersUtils.java
+++ service/src/test/java/com/github/hotire/springcore/test/MockMvcBuildersUtils.java
@@ -4,6 +4,9 @@ import com.github.hotire.springcore.exception.ExceptionHandlerController;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -12,7 +15,16 @@ public class MockMvcBuildersUtils {
     public static MockMvc mockMvc(Object... controllers) {
         return MockMvcBuilders
             .standaloneSetup(controllers)
+            .setConversionService(conversionService())
             .setControllerAdvice(new ExceptionHandlerController(new DefaultErrorAttributes()))
             .build();
+    }
+
+    public static FormattingConversionService conversionService() {
+        final DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        final FormattingConversionService conversion = new DefaultFormattingConversionService();
+        registrar.setUseIsoFormat(true);
+        registrar.registerFormatters(conversion);
+        return conversion;
     }
 }
