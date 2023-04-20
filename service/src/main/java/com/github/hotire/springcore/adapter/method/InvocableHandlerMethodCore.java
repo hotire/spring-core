98be6d93f0084e816b26ed6926fb2f5a443e931d
hotire <gngh0101@gmail.com>
2023-04-21 03:22:48 +0900

    Add InvocableHandlerMethodCore

---
+++ service/src/main/java/com/github/hotire/springcore/adapter/method/InvocableHandlerMethodCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springcore.adapter.method;
+
+/**
+ * @see org.springframework.web.method.support.InvocableHandlerMethod
+ */
+public class InvocableHandlerMethodCore {
+
+}
