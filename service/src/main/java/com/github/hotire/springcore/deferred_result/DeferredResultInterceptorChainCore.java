fc1139f4c5b0d6bf893eb58333fbb7e1aa1ec4e3
hotire <gngh0101@gmail.com>
2023-04-21 03:31:11 +0900

    triggerAfterTimeout

--- service/src/main/java/com/github/hotire/springcore/deferred_result/DeferredResultInterceptorChainCore.java
+++ service/src/main/java/com/github/hotire/springcore/deferred_result/DeferredResultInterceptorChainCore.java
@@ -1,9 +1,19 @@
 package com.github.hotire.springcore.deferred_result;
 
 
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.async.DeferredResult;
+
 /**
  * @see org.springframework.web.context.request.async.DeferredResultInterceptorChain;
  */
 public class DeferredResultInterceptorChainCore {
 
+
+    /**
+     * @see org.springframework.web.context.request.async.DeferredResultInterceptorChain#triggerAfterTimeout(NativeWebRequest, DeferredResult) 
+     */
+    public void triggerAfterTimeout(NativeWebRequest request, DeferredResult<?> deferredResult) throws Exception {
+
+    }
 }
