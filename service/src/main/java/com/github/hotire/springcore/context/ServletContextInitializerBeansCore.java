7130ed74de503341395621ccddf2a3b6673f29fa
hotire <gngh0101@gmail.com>
2023-04-22 09:09:51 +0900

    Add ServletContextInitializerBeansCore

---
+++ service/src/main/java/com/github/hotire/springcore/context/ServletContextInitializerBeansCore.java
@@ -0,0 +1,12 @@
+package com.github.hotire.springcore.context;
+
+import org.springframework.boot.web.servlet.ServletContextInitializerBeans;
+
+
+/**
+ * @see ServletContextInitializerBeans
+ * Filter, Servelt 등록
+ */
+public class ServletContextInitializerBeansCore {
+
+}
---
+++ service/src/main/java/com/github/hotire/springcore/filter/FilterCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.filter;
+import javax.servlet.Filter;
+
+
+/**
+ * @see Filter
+ */
+public class FilterCore {
+
+}
