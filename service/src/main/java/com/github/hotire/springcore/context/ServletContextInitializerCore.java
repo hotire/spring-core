e5f3743d39ee1f32b10d3d2f56b22f440cd1ad7b
hotire <gngh0101@gmail.com>
2023-04-22 09:04:41 +0900

    Add ServletContextInitializerCore

---
+++ service/src/main/java/com/github/hotire/springcore/context/ServletContextInitializerCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.context;
+
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+/**
+ * @see ServletContextInitializer
+ */
+public class ServletContextInitializerCore {
+
+}
