c86bb192fc4da84f46579a934655e577e32c22e8
hotire <gngh0101@gmail.com>
2023-04-23 15:15:26 +0900

    configure in FilterRegistrationBean

--- service/src/main/java/com/github/hotire/springcore/filter/FilterRegistrationBeanCore.java
+++ service/src/main/java/com/github/hotire/springcore/filter/FilterRegistrationBeanCore.java
@@ -1,7 +1,30 @@
 package com.github.hotire.springcore.filter;
 
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.FilterRegistration.Dynamic;
+import javax.servlet.ServletContext;
+import org.springframework.boot.web.servlet.AbstractFilterRegistrationBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+
 /**
- * @see org.springframework.boot.web.servlet.FilterRegistrationBean
+ * @see AbstractFilterRegistrationBean
+ * @see FilterRegistrationBean;
  */
 public class FilterRegistrationBeanCore {
+
+    /**
+     * @see AbstractFilterRegistrationBean#dispatcherTypes
+     */
+    private EnumSet<DispatcherType> dispatcherTypes;
+
+    /**
+     * @see org.springframework.boot.web.servlet.RegistrationBean#onStartup(ServletContext) 
+     * @see org.springframework.boot.web.servlet.DynamicRegistrationBean#register(String, ServletContext) 
+     * @see FilterRegistrationBean#configure(Dynamic) 
+     */
+    protected void configure(FilterRegistration.Dynamic registration) {
+
+    }
 }
