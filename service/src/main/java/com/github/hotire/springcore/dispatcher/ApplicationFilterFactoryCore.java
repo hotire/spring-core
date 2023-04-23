a0a81a634038d36dd9662010228c352f184d34d7
hotire <gngh0101@gmail.com>
2023-04-24 00:27:42 +0900

    Add matchDispatcher

--- service/src/main/java/com/github/hotire/springcore/dispatcher/ApplicationFilterFactoryCore.java
+++ service/src/main/java/com/github/hotire/springcore/dispatcher/ApplicationFilterFactoryCore.java
@@ -1,10 +1,12 @@
 package com.github.hotire.springcore.dispatcher;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.Servlet;
 import javax.servlet.ServletRequest;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.core.ApplicationFilterChain;
 import org.apache.catalina.core.ApplicationFilterFactory;
+import org.apache.tomcat.util.descriptor.web.FilterMap;
 
 /**
  * @see ApplicationFilterFactory
@@ -13,10 +15,17 @@ public class ApplicationFilterFactoryCore {
 
 
     /**
-     * @see ApplicationFilterFactory#createFilterChain(ServletRequest, Wrapper, Servlet) 
+     * @see ApplicationFilterFactory#createFilterChain(ServletRequest, Wrapper, Servlet)
      */
     public static ApplicationFilterChain createFilterChain(ServletRequest request,
         Wrapper wrapper, Servlet servlet) {
         return ApplicationFilterFactory.createFilterChain(request, wrapper, servlet);
+    }
+
+    /**
+     * @see ApplicationFilterFactory#matchDispatcher(FilterMap, DispatcherType) 
+     */
+    private static boolean matchDispatcher(FilterMap filterMap, DispatcherType type) {
+        return true;
     }
 }
