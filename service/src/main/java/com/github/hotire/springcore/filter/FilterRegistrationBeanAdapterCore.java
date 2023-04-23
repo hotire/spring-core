6e4649faf56dd40ac066550c6c02b55ca3e58a82
hotire <gngh0101@gmail.com>
2023-04-23 15:11:50 +0900

    Fix typo

--- service/src/main/java/com/github/hotire/springcore/filter/FilterRegistrationBeanAdapterCore.java
+++ service/src/main/java/com/github/hotire/springcore/filter/FilterRegistrationBeanAdapterCore.java
@@ -1,9 +1,23 @@
 package com.github.hotire.springcore.filter;
 
 
+import javax.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.RegistrationBean;
+
 /**
+ * @see org.springframework.boot.web.servlet.ServletContextInitializerBeans.RegistrationBeanAdapter
  * @see org.springframework.boot.web.servlet.ServletContextInitializerBeans.FilterRegistrationBeanAdapter
  */
 public class FilterRegistrationBeanAdapterCore {
+
+    /**
+     * @see org.springframework.boot.web.servlet.ServletContextInitializerBeans.FilterRegistrationBeanAdapter#createRegistrationBean(String, Filter, int)
+     */
+    public RegistrationBean createRegistrationBean(String name, Filter source, int totalNumberOfSourceBeans) {
+        FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>(source);
+        bean.setName(name);
+        return bean;
+    }
 
 }
