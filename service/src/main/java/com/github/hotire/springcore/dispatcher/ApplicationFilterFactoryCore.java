5c9cfa5282de997358986993490df4583f17539f
hotire <gngh0101@gmail.com>
2023-04-24 00:26:18 +0900

    Add createFilterChain

--- service/src/main/java/com/github/hotire/springcore/dispatcher/ApplicationFilterFactoryCore.java
+++ service/src/main/java/com/github/hotire/springcore/dispatcher/ApplicationFilterFactoryCore.java
@@ -1,10 +1,22 @@
 package com.github.hotire.springcore.dispatcher;
 
-import  org.apache.catalina.core.ApplicationFilterFactory;
+import javax.servlet.Servlet;
+import javax.servlet.ServletRequest;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.core.ApplicationFilterChain;
+import org.apache.catalina.core.ApplicationFilterFactory;
 
 /**
  * @see ApplicationFilterFactory
  */
 public class ApplicationFilterFactoryCore {
 
+
+    /**
+     * @see ApplicationFilterFactory#createFilterChain(ServletRequest, Wrapper, Servlet) 
+     */
+    public static ApplicationFilterChain createFilterChain(ServletRequest request,
+        Wrapper wrapper, Servlet servlet) {
+        return ApplicationFilterFactory.createFilterChain(request, wrapper, servlet);
+    }
 }
