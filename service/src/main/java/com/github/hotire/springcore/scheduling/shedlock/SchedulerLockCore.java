193afe2d64e4bda0318bdd96116baccccc1e70a6
hotire <gngh0101@gmail.com>
2023-04-28 10:29:26 +0800

    Add SchedulerLockCore

---
+++ service/src/main/java/com/github/hotire/springcore/scheduling/shedlock/SchedulerLockCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.scheduling.shedlock;
+
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+
+/**
+ * @see SchedulerLock
+ */
+public class SchedulerLockCore {
+
+}
