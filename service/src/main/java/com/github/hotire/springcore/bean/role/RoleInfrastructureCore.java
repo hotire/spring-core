b80b5d0dcb792af6e5c0255eb3f5cb7438046853
hotire <gngh0101@gmail.com>
2023-05-01 22:46:33 +0900

    Add RoleInfrastructureCore

---
+++ service/src/main/java/com/github/hotire/springcore/bean/role/RoleInfrastructureCore.java
@@ -0,0 +1,11 @@
+package com.github.hotire.springcore.bean.role;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Role;
+import org.springframework.stereotype.Component;
+
+@Component
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+public class RoleInfrastructureCore {
+
+}
