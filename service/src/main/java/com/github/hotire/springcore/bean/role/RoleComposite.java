e0df2443fc14349758966a55e968fdd1cee01055
hotire <gngh0101@gmail.com>
2023-05-02 01:46:26 +0900

    modify shedlock version 4.44.0

--- service/src/main/java/com/github/hotire/springcore/bean/role/RoleComposite.java
+++ service/src/main/java/com/github/hotire/springcore/bean/role/RoleComposite.java
@@ -3,13 +3,17 @@ package com.github.hotire.springcore.bean.role;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class RoleComposite {
+
+    private final ApplicationContext context;
     private final RoleInfrastructureCore roleInfrastructureCore;
+
 
     @PostConstruct
     public void init() {
