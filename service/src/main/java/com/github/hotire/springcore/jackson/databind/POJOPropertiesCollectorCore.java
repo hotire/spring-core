cafe5eaaef474bcbe0c29a7b924bb83a03680ba9
hotire <gngh0101@gmail.com>
2023-05-04 03:01:56 +0900

    collectAll in POJOPropertiesCollectorCore

--- service/src/main/java/com/github/hotire/springcore/jackson/databind/POJOPropertiesCollectorCore.java
+++ service/src/main/java/com/github/hotire/springcore/jackson/databind/POJOPropertiesCollectorCore.java
@@ -1,11 +1,33 @@
 package com.github.hotire.springcore.jackson.databind;
 
 import com.fasterxml.jackson.databind.introspect.POJOPropertiesCollector;
+import com.fasterxml.jackson.databind.introspect.POJOPropertyBuilder;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @see POJOPropertiesCollector
  */
 public interface POJOPropertiesCollectorCore {
+
+    /**
+     * @see POJOPropertiesCollector#collectAll()
+     */
+    default void collectAll() {
+        final LinkedHashMap<String, POJOPropertyBuilder> props = new LinkedHashMap<String, POJOPropertyBuilder>();
+        _addFields(props);
+        _addMethods(props);
+    }
+
+    /**
+     * @see POJOPropertiesCollector#_addFields(Map)
+     */
+    void _addFields(Map<String, POJOPropertyBuilder> props);
+
+    /**
+     * @see POJOPropertiesCollector#_addMethods(Map)
+     */
+    void _addMethods(Map<String, POJOPropertyBuilder> props);
 
     /**
      * @see POJOPropertiesCollector#_checkRenameByField(String) 
