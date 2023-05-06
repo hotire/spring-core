7c85b8597ebbc2fa85ca24576aa8e55c4196c4c5
hotire <gngh0101@gmail.com>
2023-05-06 16:34:56 +0900

    DefaultSerializerProvider.Impl

--- service/src/main/java/com/github/hotire/springcore/jackson/databind/SerializerProviderCore.java
+++ service/src/main/java/com/github/hotire/springcore/jackson/databind/SerializerProviderCore.java
@@ -1,12 +1,24 @@
 package com.github.hotire.springcore.jackson.databind;
 
+import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.DefaultSerializerProvider;
+import com.fasterxml.jackson.databind.ser.DefaultSerializerProvider.Impl;
+import com.fasterxml.jackson.databind.ser.SerializerFactory;
 
 /**
  * @see DefaultSerializerProvider
+ * @see Impl
  * @see SerializerProvider
  */
-public class SerializerProviderCore {
+public interface SerializerProviderCore {
+
+    /**
+     * @see DefaultSerializerProvider#createInstance(SerializationConfig, SerializerFactory)
+     * @see Impl#createInstance(SerializationConfig, SerializerFactory) 
+     */
+    DefaultSerializerProvider createInstance(SerializationConfig config,
+        SerializerFactory jsf);
+
 
 }
