8054336e1f9518b7e1395ae82b2fcbca6b484162
hotire <gngh0101@gmail.com>
2023-05-07 22:43:40 +0900

    serializeValue

--- service/src/main/java/com/github/hotire/springcore/jackson/databind/SerializerProviderCore.java
+++ service/src/main/java/com/github/hotire/springcore/jackson/databind/SerializerProviderCore.java
@@ -1,10 +1,12 @@
 package com.github.hotire.springcore.jackson.databind;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.DefaultSerializerProvider;
 import com.fasterxml.jackson.databind.ser.DefaultSerializerProvider.Impl;
 import com.fasterxml.jackson.databind.ser.SerializerFactory;
+import java.io.IOException;
 
 /**
  * @see DefaultSerializerProvider
@@ -15,10 +17,15 @@ public interface SerializerProviderCore {
 
     /**
      * @see DefaultSerializerProvider#createInstance(SerializationConfig, SerializerFactory)
-     * @see Impl#createInstance(SerializationConfig, SerializerFactory) 
+     * @see Impl#createInstance(SerializationConfig, SerializerFactory)
      */
     DefaultSerializerProvider createInstance(SerializationConfig config,
         SerializerFactory jsf);
+
+    /**
+     * @see DefaultSerializerProvider#serializeValue(JsonGenerator, Object)
+     */
+    void serializeValue(JsonGenerator gen, Object value) throws IOException;
 
 
 }
