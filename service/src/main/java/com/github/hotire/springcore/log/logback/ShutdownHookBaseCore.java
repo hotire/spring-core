45ae68670e4bfdd43ee01f65dce86540893f1ebe
hotire <gngh0101@gmail.com>
2023-05-11 03:15:03 +0900

    Add ShutdownHookBase

---
+++ service/src/main/java/com/github/hotire/springcore/log/logback/ShutdownHookBaseCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.log.logback;
+
+import ch.qos.logback.core.hook.ShutdownHookBase;
+
+/**
+ * @see ShutdownHookBase
+ */
+public class ShutdownHookBaseCore {
+
+}
---
+++ service/src/main/java/com/github/hotire/springcore/log/logback/ShutdownHookCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.log.logback;
+
+import ch.qos.logback.core.hook.ShutdownHook;
+
+/**
+ * @see ShutdownHook
+ */
+public class ShutdownHookCore {
+
+}
