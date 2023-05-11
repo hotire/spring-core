368ec9ec6a46491ddef8bf68e6c13a2c6ba635d1
hotire <gngh0101@gmail.com>
2023-05-12 02:50:52 +0900

    Add DelayingShutdownHook

--- service/src/main/java/com/github/hotire/springcore/log/logback/ShutdownHookBaseCore.java
+++ service/src/main/java/com/github/hotire/springcore/log/logback/ShutdownHookBaseCore.java
@@ -1,9 +1,11 @@
 package com.github.hotire.springcore.log.logback;
 
+import ch.qos.logback.core.hook.DelayingShutdownHook;
 import ch.qos.logback.core.hook.ShutdownHookBase;
 
 /**
  * @see ShutdownHookBase
+ * @see DelayingShutdownHook
  */
 public class ShutdownHookBaseCore {
 
