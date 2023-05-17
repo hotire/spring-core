5133f1d7acb1d670b91f83911482f571f022efaf
hotire <gngh0101@gmail.com>
2023-05-18 02:56:04 +0900

    Add WebServerFactoryCore

---
+++ service/src/main/java/com/github/hotire/springcore/server/WebServerFactoryCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.server;
+
+import org.springframework.boot.web.server.WebServerFactory;
+
+/**
+ * @see WebServerFactory
+ */
+public class WebServerFactoryCore {
+
+}
