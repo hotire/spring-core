46676451de1a4324fe15df3064310e38d70cf8f9
hotire <gngh0101@gmail.com>
2023-05-18 02:55:23 +0900

    Add WebServerFactoryCustomizerCore

---
+++ service/src/main/java/com/github/hotire/springcore/server/WebServerFactoryCustomizerCore.java
@@ -0,0 +1,11 @@
+package com.github.hotire.springcore.server;
+
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+
+
+/**
+ * @see WebServerFactoryCustomizer
+ */
+public class WebServerFactoryCustomizerCore {
+
+}
