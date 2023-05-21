ff4b97a9d67bc38d38c933f9675dd7c7ee71471b
hotire <gngh0101@gmail.com>
2023-05-22 00:35:44 +0900

    Add WebServerFactoryCustomizerCore

--- service/src/main/java/com/github/hotire/springcore/server/WebServerFactoryCustomizerCore.java
+++ service/src/main/java/com/github/hotire/springcore/server/WebServerFactoryCustomizerCore.java
@@ -1,10 +1,11 @@
 package com.github.hotire.springcore.server;
 
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-
+import org.springframework.boot.autoconfigure.websocket.servlet.TomcatWebSocketServletWebServerCustomizer;
 
 /**
  * @see WebServerFactoryCustomizer
+ * @see TomcatWebSocketServletWebServerCustomizer
  */
 public class WebServerFactoryCustomizerCore {
 
