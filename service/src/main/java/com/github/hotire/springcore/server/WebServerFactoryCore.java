9bc9765808544d267ec2a39041c0d4324227c96a
hotire <gngh0101@gmail.com>
2023-05-21 23:20:22 +0900

    Add TomcatServletWebServerFactory

--- service/src/main/java/com/github/hotire/springcore/server/WebServerFactoryCore.java
+++ service/src/main/java/com/github/hotire/springcore/server/WebServerFactoryCore.java
@@ -1,9 +1,11 @@
 package com.github.hotire.springcore.server;
 
 import org.springframework.boot.web.server.WebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 
 /**
  * @see WebServerFactory
+ * @see TomcatServletWebServerFactory
  */
 public class WebServerFactoryCore {
 
