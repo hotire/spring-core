7541751740beb9a5338915c45957234fa5244f02
hotire <gngh0101@gmail.com>
2023-06-02 03:17:46 +0900

    Add TomcatWebServerFactoryCustomizer

--- service/src/main/java/com/github/hotire/springcore/tomcat/ex/TomcatWebServerFactoryCustomizer.java
+++ service/src/main/java/com/github/hotire/springcore/tomcat/ex/TomcatWebServerFactoryCustomizer.java
@@ -1,9 +1,9 @@
-package com.github.hotire.springcore.tomcat;
+package com.github.hotire.springcore.tomcat.ex;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 
-public class CustomTomcatWebServerFactoryCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+public class TomcatWebServerFactoryCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
 
     @Override
     public void customize(TomcatServletWebServerFactory factory) {
