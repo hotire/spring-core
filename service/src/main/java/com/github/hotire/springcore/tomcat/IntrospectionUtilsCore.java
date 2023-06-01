51fad70d0c7a7d2395902a6f2393f7ce2eb39205
hotire <gngh0101@gmail.com>
2023-06-02 03:18:24 +0900

    Add IntrospectionUtilsCore

---
+++ service/src/main/java/com/github/hotire/springcore/tomcat/IntrospectionUtilsCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.tomcat;
+
+import org.apache.tomcat.util.IntrospectionUtils;
+
+/**
+ * @see IntrospectionUtils
+ */
+public class IntrospectionUtilsCore {
+
+}
