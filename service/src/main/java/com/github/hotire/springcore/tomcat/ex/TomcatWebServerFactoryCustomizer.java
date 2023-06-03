2529bfdda305061f7af1cfaaebae40c55e1464a9
hotire <gngh0101@gmail.com>
2023-06-03 13:00:24 +0900

    customize maxThread

--- service/src/main/java/com/github/hotire/springcore/tomcat/ex/TomcatWebServerFactoryCustomizer.java
+++ service/src/main/java/com/github/hotire/springcore/tomcat/ex/TomcatWebServerFactoryCustomizer.java
@@ -1,13 +1,24 @@
 package com.github.hotire.springcore.tomcat.ex;
 
+import org.apache.coyote.AbstractProtocol;
+import org.apache.tomcat.util.IntrospectionUtils;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 
+/**
+ * @see WebServerFactoryCustomizer
+ * @see TomcatServletWebServerFactory
+ */
 public class TomcatWebServerFactoryCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
 
+    /**
+     * @see AbstractProtocol
+     * @see IntrospectionUtils#setProperty(Object, String, String, boolean, StringBuilder) 
+     */
     @Override
     public void customize(TomcatServletWebServerFactory factory) {
         factory.addConnectorCustomizers(connector -> {
+            connector.setProperty("maxThread", "200");
         });
     }
 }
