50a2313742be328235bbae511e6ed0c62427d846
hotire <gngh0101@gmail.com>
2023-06-18 23:48:21 +0900

    getLazyResolutionProxyIfNecessary in ContextAnnotationAutowireCandidateResolver

--- service/src/main/java/com/github/hotire/springcore/lazy/ContextAnnotationAutowireCandidateResolverCore.java
+++ service/src/main/java/com/github/hotire/springcore/lazy/ContextAnnotationAutowireCandidateResolverCore.java
@@ -1,10 +1,20 @@
 package com.github.hotire.springcore.lazy;
 
+import org.springframework.beans.factory.config.DependencyDescriptor;
 import org.springframework.context.annotation.ContextAnnotationAutowireCandidateResolver;
+import org.springframework.lang.Nullable;
 
 /**
  * @see ContextAnnotationAutowireCandidateResolver
  */
 public class ContextAnnotationAutowireCandidateResolverCore {
 
+
+    /**
+     * @see ContextAnnotationAutowireCandidateResolver#getLazyResolutionProxyIfNecessary(DependencyDescriptor, String) 
+     */
+    public Object getLazyResolutionProxyIfNecessary(
+        DependencyDescriptor descriptor, @Nullable String beanName) {
+        return null;
+    }
 }
