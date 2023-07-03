38b28128cbf0f13aa069666bc4ad6db6013a5994
hotire <gngh0101@gmail.com>
2023-07-04 03:13:40 +0900

    Add AbstractCacheManager

--- service/src/main/java/com/github/hotire/springcore/cache/CacheManagerCore.java
+++ service/src/main/java/com/github/hotire/springcore/cache/CacheManagerCore.java
@@ -1,9 +1,11 @@
 package com.github.hotire.springcore.cache;
 
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.support.AbstractCacheManager;
 
 /**
  * @see CacheManager
+ * @see AbstractCacheManager
  */
 public class CacheManagerCore {
 
