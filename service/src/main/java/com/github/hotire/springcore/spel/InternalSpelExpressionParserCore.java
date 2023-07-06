bec407085b6f1b9684f17b60ed8f9eeb48d3067d
hotire <gngh0101@gmail.com>
2023-07-07 02:36:23 +0900

    Add InternalSpelExpressionParser

---
+++ service/src/main/java/com/github/hotire/springcore/spel/InternalSpelExpressionParserCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springcore.spel;
+
+/**
+ * @see org.springframework.expression.spel.standard.InternalSpelExpressionParser
+ */
+public class InternalSpelExpressionParserCore {
+
+}
