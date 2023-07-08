71a1ce5b7530ea6936ee1e11133a91accd508e9a
hotire <gngh0101@gmail.com>
2023-07-08 14:58:08 +0900

    Add SpelExpressionParserCore

---
+++ service/src/main/java/com/github/hotire/springcore/spel/SpelExpressionParserCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.spel;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+
+/**
+ * @see SpelExpressionParser
+ */
+public class SpelExpressionParserCore {
+
+}
