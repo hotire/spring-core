b67abb7e7cdee8a91e402c2edc6e4160adbb8d16
hotire <gngh0101@gmail.com>
2023-07-11 02:49:45 +0900

    Add Tokenizer.process()

--- service/src/main/java/com/github/hotire/springcore/spel/TokenizerCore.java
+++ service/src/main/java/com/github/hotire/springcore/spel/TokenizerCore.java
@@ -1,9 +1,25 @@
 package com.github.hotire.springcore.spel;
 
+import java.util.List;
 
 /**
  * @see org.springframework.expression.spel.standard.Tokenizer
  */
 public class TokenizerCore {
+
+    /**
+     * @see org.springframework.expression.spel.standard.Token;
+     */
+
+    static class Token {
+
+    }
+
+    /**
+     * @see org.springframework.expression.spel.standard.Tokenizer.Tokenizer#process()
+     */
+    public List<Token> process() {
+        return List.of();
+    }
 
 }
