f2de1c7ccac85e87ec7e01ff9166397286c07b56
hotire <gngh0101@gmail.com>
2023-07-11 02:45:44 +0900

    Add SpelExpressionParser.doParseExpression

--- service/src/main/java/com/github/hotire/springcore/spel/SpelExpressionParserCore.java
+++ service/src/main/java/com/github/hotire/springcore/spel/SpelExpressionParserCore.java
@@ -1,5 +1,9 @@
 package com.github.hotire.springcore.spel;
+import org.springframework.expression.ParseException;
+import org.springframework.expression.ParserContext;
+import org.springframework.expression.spel.standard.SpelExpression;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.lang.Nullable;
 
 
 /**
@@ -7,4 +11,10 @@ import org.springframework.expression.spel.standard.SpelExpressionParser;
  */
 public class SpelExpressionParserCore {
 
+    /**
+     * @see SpelExpressionParser#doParseExpression(String, ParserContext)
+     */
+    protected SpelExpression doParseExpression(String expressionString, @Nullable ParserContext context) throws ParseException {
+        return null;
+    }
 }
