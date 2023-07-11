b3c00d139d877835034a52dda26c7b693b3a313b
hotire <gngh0101@gmail.com>
2023-07-12 00:55:36 +0900

    Fix typo

--- service/src/main/java/com/github/hotire/springcore/spel/TokenizerCore.java
+++ service/src/main/java/com/github/hotire/springcore/spel/TokenizerCore.java
@@ -16,7 +16,7 @@ public class TokenizerCore {
     }
 
     /**
-     * @see org.springframework.expression.spel.standard.Tokenizer.Tokenizer#process()
+     * @see org.springframework.expression.spel.standard.Tokenizer#process()
      */
     public List<Token> process() {
         return List.of();
