a51a2a53c4149cc73c24762f092ecdec28defdc4
hotire <gngh0101@gmail.com>
2023-08-29 01:21:42 +0900

    Add LifecyclePropertiesCore

---
+++ service/src/main/java/com/github/hotire/springcore/shutdown/LifecyclePropertiesCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.shutdown;
+
+import org.springframework.boot.autoconfigure.context.LifecycleProperties;
+
+/**
+ * @see LifecycleProperties
+ */
+public class LifecyclePropertiesCore {
+
+}
