f41f105576a2957657cb35d363abab901f0ccf79
hotire <gngh0101@gmail.com>
2023-08-30 02:57:23 +0900

    Add ServerPropertiesCore

---
+++ service/src/main/java/com/github/hotire/springcore/shutdown/ServerPropertiesCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.shutdown;
+
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+
+/**
+ * @see ServerProperties
+ */
+public class ServerPropertiesCore {
+
+}
