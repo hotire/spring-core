20e30e473e1e1ab1a27c2885097a12ca47b95077
hotire <gngh0101@gmail.com>
2023-10-04 01:09:09 +0900

    Add TaskExecutorCore

---
+++ service/src/main/java/com/github/hotire/springcore/concurrent/spring/TaskExecutorCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.concurrent.spring;
+
+import org.springframework.core.task.TaskExecutor;
+
+/**
+ * @see TaskExecutor
+ */
+public class TaskExecutorCore {
+
+}
