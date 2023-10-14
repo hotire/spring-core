2f881022b2ce46affd4f09e6e918922ba1865238
hotire <gngh0101@gmail.com>
2023-10-14 22:10:52 +0900

    Add ExecutorConfigurationSupportCore

---
+++ service/src/main/java/com/github/hotire/springcore/concurrent/spring/ExecutorConfigurationSupportCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.concurrent.spring;
+
+import org.springframework.scheduling.concurrent.ExecutorConfigurationSupport;
+
+/**
+ * @see ExecutorConfigurationSupport
+ */
+public class ExecutorConfigurationSupportCore {
+
+}
