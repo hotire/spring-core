be7295f4057e36a30363ec78443af60871436ed4
hotire <gngh0101@gmail.com>
2023-10-16 00:46:43 +0900

    ExecutorConfigurationSupport awaitTerminationIfNecessary

--- service/src/main/java/com/github/hotire/springcore/concurrent/executor/ExecutorServiceCore.java
+++ service/src/main/java/com/github/hotire/springcore/concurrent/executor/ExecutorServiceCore.java
@@ -1,11 +1,25 @@
 package com.github.hotire.springcore.executor;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @see ExecutorService
  * @see AbstractExecutorService
  */
-public class ExecutorServiceCore {
+public interface ExecutorServiceCore {
 
+
+    /**
+     * @see ExecutorService#shutdown()
+     */
+    void shutdown();
+
+
+    /**
+     * @see ExecutorService#awaitTermination(long, TimeUnit) 
+     */
+    boolean awaitTermination(long timeout, TimeUnit unit)
+        throws InterruptedException;
 }
--- service/src/main/java/com/github/hotire/springcore/concurrent/spring/ExecutorConfigurationSupportCore.java
+++ service/src/main/java/com/github/hotire/springcore/concurrent/spring/ExecutorConfigurationSupportCore.java
@@ -1,5 +1,6 @@
 package com.github.hotire.springcore.concurrent.spring;
 
+import java.util.concurrent.ExecutorService;
 import org.springframework.scheduling.concurrent.ExecutorConfigurationSupport;
 
 /**
@@ -11,4 +12,20 @@ public class ExecutorConfigurationSupportCore {
      * @see ExecutorConfigurationSupport#waitForTasksToCompleteOnShutdown
      */
     private boolean waitForTasksToCompleteOnShutdown = false;
+
+
+    /**
+     * @see ExecutorConfigurationSupport#shutdown()
+     */
+    public void shutdown() {
+
+    }
+
+    /**
+     * @see ExecutorConfigurationSupport#awaitTerminationIfNecessary(ExecutorService)
+     */
+    public void awaitTerminationIfNecessary(ExecutorService executorService) {
+
+    }
+
 }
