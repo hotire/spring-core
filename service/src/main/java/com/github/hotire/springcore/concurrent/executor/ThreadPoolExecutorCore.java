e29da1524249806e61e257c03916621edf8a9312
hotire <gngh0101@gmail.com>
2023-10-18 01:17:12 +0900

    Add Worker

--- service/src/main/java/com/github/hotire/springcore/concurrent/executor/ThreadPoolExecutorCore.java
+++ service/src/main/java/com/github/hotire/springcore/concurrent/executor/ThreadPoolExecutorCore.java
@@ -1,10 +1,18 @@
 package com.github.hotire.springcore.executor;
 
+import java.util.HashSet;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @see ThreadPoolExecutor
  */
 public class ThreadPoolExecutorCore {
+
+    private final HashSet<Worker> workers = new HashSet<>();
+
+
+    static class Worker {
+
+    }
 
 }
