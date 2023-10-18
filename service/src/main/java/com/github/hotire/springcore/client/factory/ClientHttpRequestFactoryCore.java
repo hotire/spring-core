ce558340b6667abf15996006ba115507dfa28409
hotire <gngh0101@gmail.com>
2023-10-19 00:55:47 +0900

    Add ClientHttpRequestFactoryCore

---
+++ service/src/main/java/com/github/hotire/springcore/client/factory/ClientHttpRequestFactoryCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.client.factory;
+
+import org.springframework.http.client.ClientHttpRequestFactory;
+
+/**
+ * @see ClientHttpRequestFactory
+ */
+public class ClientHttpRequestFactoryCore {
+
+}
