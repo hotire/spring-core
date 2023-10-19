a79af47e1951e1500f4d1ef3606893fe092a4e01
hotire <gngh0101@gmail.com>
2023-10-20 01:40:08 +0900

    Add CloseableHttpClient, InternalHttpClient

--- service/src/main/java/com/github/hotire/springcore/client/apache/HttpClientCore.java
+++ service/src/main/java/com/github/hotire/springcore/client/apache/HttpClientCore.java
@@ -1,9 +1,12 @@
 package com.github.hotire.springcore.client.apache;
 
 import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
 
 /**
  * @see HttpClient
+ * @see CloseableHttpClient
+ * @see org.apache.http.impl.client.InternalHttpClient;
  */
 public class HttpClientCore {
 
