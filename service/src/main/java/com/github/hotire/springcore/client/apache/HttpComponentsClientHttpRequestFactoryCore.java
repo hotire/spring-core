670b1b9844d0d4e503973c77446aca7e3f067136
hotire <gngh0101@gmail.com>
2023-10-21 13:39:29 +0900

    HttpComponentsClientHttpRequestFactory#createRequest

--- service/src/main/java/com/github/hotire/springcore/client/apache/HttpComponentsClientHttpRequestFactoryCore.java
+++ service/src/main/java/com/github/hotire/springcore/client/apache/HttpComponentsClientHttpRequestFactoryCore.java
@@ -1,6 +1,10 @@
 package com.github.hotire.springcore.client.apache;
 
+import java.io.IOException;
+import java.net.URI;
 import org.apache.http.client.HttpClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 /**
@@ -10,6 +14,13 @@ public class HttpComponentsClientHttpRequestFactoryCore {
 
     private HttpClient httpClient;
 
+
+    /**
+     * @see HttpComponentsClientHttpRequestFactory#createRequest(URI, HttpMethod)
+     */
+    public ClientHttpRequest createRequest(URI uri, HttpMethod httpMethod) throws IOException {
+        return null;
+    }
 
     /**
      * @see HttpComponentsClientHttpRequestFactory#getHttpClient()
