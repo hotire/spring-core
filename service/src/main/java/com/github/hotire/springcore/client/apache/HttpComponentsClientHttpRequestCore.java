f560c5ee81a28e2508eb0c3340c298a488f94637
hotire <gngh0101@gmail.com>
2023-10-22 23:42:33 +0900

    Add HttpComponentsClientHttpRequestCore

---
+++ service/src/main/java/com/github/hotire/springcore/client/apache/HttpComponentsClientHttpRequestCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springcore.client.apache;
+
+
+/**
+ * @see org.springframework.http.client.HttpComponentsClientHttpRequest;
+ */
+public class HttpComponentsClientHttpRequestCore {
+
+}
