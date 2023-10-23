ec77b8f899d8942a5b7b029cb94753fb485c9cf5
hotire <gngh0101@gmail.com>
2023-10-24 03:19:13 +0900

    HttpClientConnectionManager#requestConnection(HttpRoute, Object)

--- service/src/main/java/com/github/hotire/springcore/client/apache/conn/HttpClientConnectionManagerCore.java
+++ service/src/main/java/com/github/hotire/springcore/client/apache/conn/HttpClientConnectionManagerCore.java
@@ -1,10 +1,16 @@
 package com.github.hotire.springcore.client.apache.conn;
 
+import org.apache.http.conn.ConnectionRequest;
 import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.conn.routing.HttpRoute;
 
 /**
  * @see HttpClientConnectionManager
  */
-public class HttpClientConnectionManagerCore {
+public interface HttpClientConnectionManagerCore {
 
+    /** 
+     * @see HttpClientConnectionManager#requestConnection(HttpRoute, Object) 
+     */
+    ConnectionRequest requestConnection(HttpRoute route, Object state);
 }
