7c20627e5e173abfa8a5fbda96fd32a843027b56
hotire <gngh0101@gmail.com>
2023-10-25 02:46:44 +0900

    Add ClientExecChainCore

---
+++ service/src/main/java/com/github/hotire/springcore/client/apache/execchain/ClientExecChainCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.client.apache.execchain;
+
+import org.apache.http.impl.execchain.ClientExecChain;
+
+/**
+ * @see ClientExecChain
+ */
+public class ClientExecChainCore {
+
+}
