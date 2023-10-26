4b64a3052788f907ebb3e7fbd52c5dc7e6964aa2
hotire <gngh0101@gmail.com>
2023-10-26 13:05:38 +0900

    ClientExecChain#execute

--- service/src/main/java/com/github/hotire/springcore/client/apache/execchain/ClientExecChainCore.java
+++ service/src/main/java/com/github/hotire/springcore/client/apache/execchain/ClientExecChainCore.java
@@ -1,10 +1,26 @@
 package com.github.hotire.springcore.client.apache.execchain;
 
+import java.io.IOException;
+import org.apache.http.HttpException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpExecutionAware;
+import org.apache.http.client.methods.HttpRequestWrapper;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.routing.HttpRoute;
 import org.apache.http.impl.execchain.ClientExecChain;
 
 /**
  * @see ClientExecChain
  */
-public class ClientExecChainCore {
+public interface ClientExecChainCore {
+
+    /**
+     * @see ClientExecChain#execute(HttpRoute, HttpRequestWrapper, HttpClientContext, HttpExecutionAware) 
+     */
+    CloseableHttpResponse execute(
+        HttpRoute route,
+        HttpRequestWrapper request,
+        HttpClientContext clientContext,
+        HttpExecutionAware execAware) throws IOException, HttpException;
 
 }
