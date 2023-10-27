8afc5ecbdbc341be8b4f0477a03229a23925a014
hotire <gngh0101@gmail.com>
2023-10-27 20:16:10 +0900

    Add MinimalClientExecCore

---
+++ service/src/main/java/com/github/hotire/springcore/client/apache/execchain/MinimalClientExecCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.client.apache.execchain;
+
+import org.apache.http.impl.execchain.MinimalClientExec;
+
+/**
+ * @see MinimalClientExec
+ */
+public class MinimalClientExecCore {
+
+}
