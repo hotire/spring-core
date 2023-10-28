24d83c7ecf6bc3ce24cddab0e15a65cdf81685a3
hotire <gngh0101@gmail.com>
2023-10-28 09:19:44 +0900

    Add MinimalClientExec#execute

--- service/src/main/java/com/github/hotire/springcore/client/apache/execchain/MinimalClientExecCore.java
+++ service/src/main/java/com/github/hotire/springcore/client/apache/execchain/MinimalClientExecCore.java
@@ -1,10 +1,29 @@
 package com.github.hotire.springcore.client.apache.execchain;
 
+import java.io.IOException;
+import org.apache.http.HttpException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpExecutionAware;
+import org.apache.http.client.methods.HttpRequestWrapper;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.impl.execchain.ClientExecChain;
 import org.apache.http.impl.execchain.MinimalClientExec;
 
 /**
  * @see MinimalClientExec
  */
 public class MinimalClientExecCore {
+
+    /**
+     * @see MinimalClientExec#execute(HttpRoute, HttpRequestWrapper, HttpClientContext, HttpExecutionAware)
+     */
+    public CloseableHttpResponse execute(
+        HttpRoute route,
+        HttpRequestWrapper request,
+        HttpClientContext clientContext,
+        HttpExecutionAware execAware) throws IOException, HttpException {
+        return null;
+    }
 
 }
