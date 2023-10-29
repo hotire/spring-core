7d2b4aaa781595d9541a12212e02ab277ee7556c
hotire <gngh0101@gmail.com>
2023-10-29 13:20:33 +0900

    Add connManager to MinimalClientExecCore

--- service/src/main/java/com/github/hotire/springcore/client/apache/execchain/MinimalClientExecCore.java
+++ service/src/main/java/com/github/hotire/springcore/client/apache/execchain/MinimalClientExecCore.java
@@ -1,19 +1,24 @@
 package com.github.hotire.springcore.client.apache.execchain;
 
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpExecutionAware;
 import org.apache.http.client.methods.HttpRequestWrapper;
 import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.ConnectionRequest;
+import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.conn.routing.HttpRoute;
-import org.apache.http.impl.execchain.ClientExecChain;
 import org.apache.http.impl.execchain.MinimalClientExec;
 
 /**
  * @see MinimalClientExec
  */
+@RequiredArgsConstructor
 public class MinimalClientExecCore {
+
+    private final HttpClientConnectionManager connManager;
 
     /**
      * @see MinimalClientExec#execute(HttpRoute, HttpRequestWrapper, HttpClientContext, HttpExecutionAware)
@@ -23,6 +28,7 @@ public class MinimalClientExecCore {
         HttpRequestWrapper request,
         HttpClientContext clientContext,
         HttpExecutionAware execAware) throws IOException, HttpException {
+        final ConnectionRequest connRequest = connManager.requestConnection(route, null);
         return null;
     }
 
