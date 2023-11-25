aa5bcf7bebfe1338dc75026745436a3bd39cade9
hotire <gngh0101@gmail.com>
2023-11-25 21:08:07 +0900

    Add ContentCachingResponseWrapperCore

---
+++ service/src/main/java/com/github/hotire/springcore/filter/response/ContentCachingResponseWrapperCore.java
@@ -0,0 +1,11 @@
+package com.github.hotire.springcore.filter.response;
+
+
+import org.springframework.web.util.ContentCachingResponseWrapper
+
+/**
+ * @see ContentCachingResponseWrapper
+ */
+public class ContentCachingResponseWrapperCore {
+
+}
