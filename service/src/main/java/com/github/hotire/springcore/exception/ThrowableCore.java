8242d1037cd62a373fa88263925dee95cf51e67e
hotire <gngh0101@gmail.com>
2023-12-02 15:11:23 +0900

    Add ThrowableCore

---
+++ service/src/main/java/com/github/hotire/springcore/exception/ThrowableCore.java
@@ -0,0 +1,15 @@
+package com.github.hotire.springcore.exception;
+
+
+/**
+ * @see Throwable
+ */
+public class ThrowableCore {
+
+    /**
+     * @see Throwable#fillInStackTrace()
+     */
+    public synchronized Throwable fillInStackTrace() {
+        return null;
+    }
+}
