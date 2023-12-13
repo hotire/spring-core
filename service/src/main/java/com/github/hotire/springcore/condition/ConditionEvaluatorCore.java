240b644d31d0ab640086292b9a33cc82832a024c
hotire <gngh0101@gmail.com>
2023-12-13 14:56:16 +0900

    Update ConditionEvaluatorCore

--- service/src/main/java/com/github/hotire/springcore/condition/ConditionEvaluatorCore.java
+++ service/src/main/java/com/github/hotire/springcore/condition/ConditionEvaluatorCore.java
@@ -1,8 +1,25 @@
 package com.github.hotire.springcore.condition;
 
+import org.springframework.context.annotation.ConfigurationCondition.ConfigurationPhase;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.lang.Nullable;
+
 /**
  * @see org.springframework.context.annotation.ConditionEvaluator
  */
 public class ConditionEvaluatorCore {
 
+    /**
+     * @see org.springframework.context.annotation.ConditionEvaluator#shouldSkip(AnnotatedTypeMetadata) 
+     */
+    public boolean shouldSkip(AnnotatedTypeMetadata metadata) {
+        return shouldSkip(metadata, null);
+    }
+
+    /**
+     * @see org.springframework.context.annotation.ConditionEvaluator#shouldSkip(AnnotatedTypeMetadata, ConfigurationPhase) 
+     */
+    public boolean shouldSkip(@Nullable AnnotatedTypeMetadata metadata, @Nullable ConfigurationPhase phase) {
+        return false;
+    }
 }
