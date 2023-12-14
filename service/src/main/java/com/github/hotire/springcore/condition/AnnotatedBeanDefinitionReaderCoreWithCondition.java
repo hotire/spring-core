6844aa21a63fef598fd465fb94ea520d4dd6e319
hotire <gngh0101@gmail.com>
2023-12-14 10:53:51 +0900

    Add AnnotatedBeanDefinitionReaderCoreWithCondition

---
+++ service/src/main/java/com/github/hotire/springcore/condition/AnnotatedBeanDefinitionReaderCoreWithCondition.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springcore.condition;
+
+/**
+ * @see org.springframework.context.annotation.AnnotatedBeanDefinitionReader
+ */
+public class AnnotatedBeanDefinitionReaderCoreWithCondition {
+
+}
