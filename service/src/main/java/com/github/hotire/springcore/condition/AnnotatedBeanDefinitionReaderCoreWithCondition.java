1097e7870bc257e682786cf417bd31b301d7428d
hotire <gngh0101@gmail.com>
2023-12-16 17:49:43 +0900

    Update AnnotatedBeanDefinitionReaderCoreWithCondition

--- service/src/main/java/com/github/hotire/springcore/condition/AnnotatedBeanDefinitionReaderCoreWithCondition.java
+++ service/src/main/java/com/github/hotire/springcore/condition/AnnotatedBeanDefinitionReaderCoreWithCondition.java
@@ -2,10 +2,10 @@ package com.github.hotire.springcore.condition;
 
 import java.lang.annotation.Annotation;
 import java.util.function.Supplier;
+import org.springframework.beans.factory.annotation.AnnotatedGenericBeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinitionCustomizer;
-import org.springframework.lang.Nullable;
-
 import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
+import org.springframework.lang.Nullable;
 
 /**
  * @see AnnotatedBeanDefinitionReader
@@ -13,12 +13,20 @@ import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
 public class AnnotatedBeanDefinitionReaderCoreWithCondition {
 
     /**
-     * @see AnnotatedBeanDefinitionReader#doRegisterBean(Class, String, Class[], Supplier, BeanDefinitionCustomizer[]) 
+     * @see AnnotatedBeanDefinitionReader#conditionEvaluator
+     */
+    private ConditionEvaluatorCore conditionEvaluator;
+
+    /**
+     * @see AnnotatedBeanDefinitionReader#doRegisterBean(Class, String, Class[], Supplier, BeanDefinitionCustomizer[])
      */
     private <T> void doRegisterBean(Class<T> beanClass, @Nullable String name,
         @Nullable Class<? extends Annotation>[] qualifiers, @Nullable Supplier<T> supplier,
         @Nullable BeanDefinitionCustomizer[] customizers) {
-        
+        AnnotatedGenericBeanDefinition abd = new AnnotatedGenericBeanDefinition(beanClass);
+        if (this.conditionEvaluator.shouldSkip(abd.getMetadata())) {
+            return;
+        }
     }
 
 }
