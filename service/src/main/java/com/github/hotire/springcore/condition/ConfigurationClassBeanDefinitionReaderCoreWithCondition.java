d1f4a88b6a0949fef5970bbb1d2f982cf8cee182
hotire <gngh0101@gmail.com>
2023-12-17 19:14:37 +0900

    Add ConfigurationClassBeanDefinitionReaderCoreWithCondition

---
+++ service/src/main/java/com/github/hotire/springcore/condition/ConfigurationClassBeanDefinitionReaderCoreWithCondition.java
@@ -0,0 +1,9 @@
+
+package com.github.hotire.springcore.condition;
+
+/**
+ * @see org.springframework.context.annotation.ConfigurationClassBeanDefinitionReader
+ */
+public class ConfigurationClassBeanDefinitionReaderCoreWithCondition {
+
+}
