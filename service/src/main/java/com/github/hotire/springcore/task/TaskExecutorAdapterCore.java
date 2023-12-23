8cd3ae88483b0010ed166c55936f39bcc7f89e79
hotire <gngh0101@gmail.com>
2023-12-23 10:29:19 +0900

    Add TaskExecutorAdapterCore

---
+++ service/src/main/java/com/github/hotire/springcore/task/TaskExecutorAdapterCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.task;
+
+import org.springframework.core.task.support.TaskExecutorAdapter;
+
+/**
+ * @see TaskExecutorAdapter
+ */
+public class TaskExecutorAdapterCore {
+
+}
