9e2e07216aa49499d17118ff32fb5933eefda45b
hotire <gngh0101@gmail.com>
2023-12-24 12:19:01 +0900

    Add TaskExecutorAdapter#concurrentExecutor

--- service/src/main/java/com/github/hotire/springcore/task/TaskExecutorAdapterCore.java
+++ service/src/main/java/com/github/hotire/springcore/task/TaskExecutorAdapterCore.java
@@ -1,10 +1,21 @@
 package com.github.hotire.springcore.task;
 
+import java.util.concurrent.Executor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.task.support.TaskExecutorAdapter;
+import org.springframework.core.task.AsyncTaskExecutor;
+
 
 /**
+ * @see AsyncTaskExecutor
  * @see TaskExecutorAdapter
  */
+@RequiredArgsConstructor
 public class TaskExecutorAdapterCore {
+
+    /**
+     * @see TaskExecutorAdapter#concurrentExecutor
+     */
+    private final Executor concurrentExecutor;
 
 }
