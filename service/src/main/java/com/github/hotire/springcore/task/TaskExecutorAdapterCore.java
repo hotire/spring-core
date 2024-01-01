f8545d362f3013383549b99fa963861ed1cd971d
hotire <gngh0101@gmail.com>
2024-01-01 23:46:09 +0900

    Update TaskExecutorAdapterCore

--- service/src/main/java/com/github/hotire/springcore/task/TaskExecutorAdapterCore.java
+++ service/src/main/java/com/github/hotire/springcore/task/TaskExecutorAdapterCore.java
@@ -1,7 +1,10 @@
 package com.github.hotire.springcore.task;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Future;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.task.TaskRejectedException;
 import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.core.task.AsyncTaskExecutor;
 
@@ -17,5 +20,18 @@ public class TaskExecutorAdapterCore {
      * @see TaskExecutorAdapter#concurrentExecutor
      */
     private final Executor concurrentExecutor;
+    
+    /**
+     * @see TaskExecutorAdapter#submit(Runnable)
+     */
+    public Future<?> submit(Runnable task) {
+        return null;
+    }
 
+    /**
+     * @see TaskExecutorAdapter#submit(Callable) 
+     */
+    public <T> Future<T> submit(Callable<T> task) {
+        return null;
+    }
 }
