79b3106abae062feddff569d4bb6f0df8c3571e4
hotire <gngh0101@gmail.com>
2024-01-24 02:56:00 +0900

    Add SecureRandomSpiCore

--- service/src/main/java/com/github/hotire/springcore/random/SecureRandomCore.java
+++ service/src/main/java/com/github/hotire/springcore/random/SecureRandomCore.java
@@ -1,10 +1,49 @@
 package com.github.hotire.springcore.random;
 
 import java.security.SecureRandom;
+import java.security.SecureRandomSpi;
 
 /**
  * @see SecureRandom
  */
 public class SecureRandomCore {
 
+    /**
+     * @see SecureRandom#secureRandomSpi
+     */
+    private SecureRandomSpi secureRandomSpi;
+
+    private boolean threadSafe;
+
+
+    /**
+     * @see SecureRandom#nextInt(int)
+     */
+    public int nextInt(int bound) {
+        return next(31);
+    }
+
+    /**
+     * @see SecureRandom#next(int)
+     */
+    protected final int next(int numBits) {
+        int numBytes = (numBits+7)/8;
+        byte[] b = new byte[numBytes];
+        nextBytes(b);
+        return 0;
+    }
+
+    /**
+     * @see SecureRandom#nextBytes(byte[])
+     * @see SecureRandomSpi#engineNextBytes(byte[]) 
+     */
+    public void nextBytes(byte[] bytes) {
+        if (threadSafe) {
+//            secureRandomSpi.engineNextBytes(bytes);
+        } else {
+            synchronized (this) {
+//                secureRandomSpi.engineNextBytes(bytes);
+            }
+        }
+    }
 }
---
+++ service/src/main/java/com/github/hotire/springcore/random/SecureRandomSpiCore.java
@@ -0,0 +1,11 @@
+package com.github.hotire.springcore.random;
+
+import java.security.SecureRandomSpi;
+
+/**
+ * @see SecureRandomSpi
+ * @see sun.security.provider.NativePRNG;
+ */
+public class SecureRandomSpiCore {
+
+}
