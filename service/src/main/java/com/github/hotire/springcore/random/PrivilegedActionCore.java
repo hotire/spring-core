f41657b6eb747f5cfabf364f0f10ee403c803705
hotire <gngh0101@gmail.com>
2024-02-01 01:35:15 +0900

    Update PrivilegedActionCore

--- service/src/main/java/com/github/hotire/springcore/random/PrivilegedActionCore.java
+++ service/src/main/java/com/github/hotire/springcore/random/PrivilegedActionCore.java
@@ -1,10 +1,17 @@
 package com.github.hotire.springcore.random;
 
 import java.security.PrivilegedAction;
+import java.security.AccessController;
+
 
 /**
  * @see PrivilegedAction
+ * @see AccessController
  */
-public class PrivilegedActionCore {
+public interface PrivilegedActionCore<T> {
 
+    /**
+     * @see AccessController#doPrivileged(PrivilegedAction) 
+     */
+    T run();
 }
