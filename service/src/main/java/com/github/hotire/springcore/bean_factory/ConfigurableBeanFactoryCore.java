db2bd513137bad73e5fd3cb7d73904416a9fef05
hotire <gngh0101@gmail.com>
2024-02-17 21:45:22 +0900

    Update ConfigurableBeanFactoryCore

--- service/src/main/java/com/github/hotire/springcore/bean_factory/ConfigurableBeanFactoryCore.java
+++ service/src/main/java/com/github/hotire/springcore/bean_factory/ConfigurableBeanFactoryCore.java
@@ -1,16 +1,26 @@
 package com.github.hotire.springcore.bean_factory;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.beans.factory.config.Scope;
+import org.springframework.beans.factory.support.AbstractBeanFactory;
 
 /**
  * @see ConfigurableBeanFactory
+ * @see AbstractBeanFactory
  */
 public interface ConfigurableBeanFactoryCore {
 
+
+    /**
+     * @see AbstractBeanFactory#scopes
+     */
+    Map<String, Scope> scopes = new LinkedHashMap<>(8);
+
     /**
      * @see ConfigurableBeanFactory#registerScope(String, Scope) 
-     * @see org.springframework.beans.factory.support.AbstractBeanFactory#registerScope(String, Scope) 
+     * @see AbstractBeanFactory#registerScope(String, Scope)
      */
     void registerScope(String scopeName, Scope scope);
 }
