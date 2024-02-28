a3acc0c21964add4e12c8b2abe1846864a8325b6
hotire <gngh0101@gmail.com>
2024-02-29 01:54:48 +0900

    Add ClassMetadata

--- service/src/main/java/com/github/hotire/springcore/condition/AnnotationMetadataCore.java
+++ service/src/main/java/com/github/hotire/springcore/condition/AnnotationMetadataCore.java
@@ -1,9 +1,10 @@
 package com.github.hotire.springcore.condition;
 
 import org.springframework.core.type.AnnotationMetadata;
-
+import org.springframework.core.type.ClassMetadata;
 /**
  * @see AnnotationMetadata
+ * @see ClassMetadata
  */
 public class AnnotationMetadataCore {
 
