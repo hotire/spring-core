66ded1d0aecd088b3a4269e00b55d1fdf3031f79
hotire <gngh0101@gmail.com>
2024-03-01 22:05:06 +0900

    Add org.springframework.core.type.classreading.SimpleAnnotationMetadata

--- service/src/main/java/com/github/hotire/springcore/condition/AnnotationMetadataCore.java
+++ service/src/main/java/com/github/hotire/springcore/condition/AnnotationMetadataCore.java
@@ -2,10 +2,18 @@ package com.github.hotire.springcore.condition;
 
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.core.type.ClassMetadata;
+
 /**
  * @see AnnotationMetadata
  * @see ClassMetadata
+ * @see org.springframework.core.type.classreading.SimpleAnnotationMetadata
  */
-public class AnnotationMetadataCore {
+public interface AnnotationMetadataCore {
+
+    /**
+     * @see org.springframework.core.type.classreading.SimpleAnnotationMetadata#getClassName
+     * @return
+     */
+    String getClassName();
 
 }
