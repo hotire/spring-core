c2830ea3fde59e7673850b1cbc363b6b2808d793
hotire <gngh0101@gmail.com>
2024-03-09 12:53:45 +0900

    Update BeanDefinitionCore

--- service/src/main/java/com/github/hotire/springcore/bean/definition/BeanDefinitionCore.java
+++ service/src/main/java/com/github/hotire/springcore/bean/definition/BeanDefinitionCore.java
@@ -1,11 +1,21 @@
 package com.github.hotire.springcore.bean.definition;
 
+import java.util.function.Supplier;
 import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
 
 /**
  * @see BeanDefinition
  * @see AnnotatedBeanDefinition
+ * @see AbstractBeanDefinition
+ * @see GenericBeanDefinition
  */
 public class BeanDefinitionCore {
+
+    /**
+     * @see AbstractBeanDefinition#instanceSupplier
+     */
+    private Supplier<?> instanceSupplier;
 }
