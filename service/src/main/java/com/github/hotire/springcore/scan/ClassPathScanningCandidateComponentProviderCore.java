20196feda855095c0c162ef9eba5b6f5f5d573a6
hotire <gngh0101@gmail.com>
2024-03-12 02:41:34 +0900

    Add ClassPathScanningCandidateComponentProviderCore

---
+++ service/src/main/java/com/github/hotire/springcore/scan/ClassPathScanningCandidateComponentProviderCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.scan;
+
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+
+/**
+ * @see ClassPathScanningCandidateComponentProvider
+ */
+public class ClassPathScanningCandidateComponentProviderCore {
+
+}
