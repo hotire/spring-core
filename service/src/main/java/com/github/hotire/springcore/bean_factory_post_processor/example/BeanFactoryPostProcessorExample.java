a755364295f57fd492b3d62c0b56d9159c420965
hotire <gngh0101@gmail.com>
2024-03-12 02:40:23 +0900

    Add BeanFactoryPostProcessorExample

---
+++ service/src/main/java/com/github/hotire/springcore/bean_factory_post_processor/example/BeanFactoryPostProcessorExample.java
@@ -0,0 +1,25 @@
+package com.github.hotire.springcore.bean_factory_post_processor.example;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanFactoryPostProcessorExample implements BeanDefinitionRegistryPostProcessor {
+
+    private BeanDefinitionRegistry registry;
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
+        throws BeansException {
+
+    }
+
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry)
+        throws BeansException {
+        this.registry = registry;
+    }
+}
