0dc11e631b99eabcbb36ce8c15a1fe1826b7c1a7
hotire <gngh0101@gmail.com>
2024-03-18 01:54:52 +0900

    Update

--- service/src/main/java/com/github/hotire/springcore/bean_factory_post_processor/example/SimpleBeanFactoryPostProcessor.java
+++ service/src/main/java/com/github/hotire/springcore/bean_factory_post_processor/example/SimpleBeanFactoryPostProcessor.java
@@ -4,9 +4,10 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 public class SimpleBeanFactoryPostProcessor implements BeanDefinitionRegistryPostProcessor {
 
     private BeanDefinitionRegistry registry;
