14ee66f5d846aaca572c1f7f50c5a8676a9bbc97
hotire <gngh0101@gmail.com>
2024-03-17 23:15:25 +0900

    Update SimpleClassPathScanningCandidateComponentProvider

--- service/src/main/java/com/github/hotire/springcore/bean_factory_post_processor/example/SimpleClassPathScanningCandidateComponentProvider.java
+++ service/src/main/java/com/github/hotire/springcore/bean_factory_post_processor/example/SimpleClassPathScanningCandidateComponentProvider.java
@@ -1,7 +1,17 @@
 package com.github.hotire.springcore.bean_factory_post_processor.example;
 
 
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
 
 public class SimpleClassPathScanningCandidateComponentProvider extends ClassPathScanningCandidateComponentProvider {
+
+    public SimpleClassPathScanningCandidateComponentProvider() {
+        addIncludeFilter(new AnnotationTypeFilter(SimpleService.class));
+    }
+
+    protected boolean isCandidateComponent(AnnotatedBeanDefinition beanDefinition) {
+        return beanDefinition.getMetadata().isInterface();
+    }
 }
---
+++ service/src/main/java/com/github/hotire/springcore/bean_factory_post_processor/example/SimpleService.java
@@ -0,0 +1,13 @@
+package com.github.hotire.springcore.bean_factory_post_processor.example;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SimpleService {
+
+}
