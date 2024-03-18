f75a68a1136ce8ddd3deda6dfd989ec4b2222f80
hotire <gngh0101@gmail.com>
2024-03-19 02:49:56 +0900

    Update beanDefinitions

--- service/src/main/java/com/github/hotire/springcore/bean_factory_post_processor/example/SimpleBeanFactoryPostProcessor.java
+++ service/src/main/java/com/github/hotire/springcore/bean_factory_post_processor/example/SimpleBeanFactoryPostProcessor.java
@@ -1,9 +1,13 @@
 package com.github.hotire.springcore.bean_factory_post_processor.example;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -15,7 +19,9 @@ public class SimpleBeanFactoryPostProcessor implements BeanDefinitionRegistryPos
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory)
         throws BeansException {
-
+        final List<BeanDefinition> beanDefinitions = AutoConfigurationPackages.get(beanFactory).stream()
+            .flatMap( basePackage -> provider.findCandidateComponents(basePackage).stream())
+            .collect(Collectors.toList());
     }
 
     @Override
