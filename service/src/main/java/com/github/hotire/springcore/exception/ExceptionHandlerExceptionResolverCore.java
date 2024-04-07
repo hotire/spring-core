ddd812371ea7e27d53bdd8d5ec02f6b370b109d1
hotire <gngh0101@gmail.com>
2024-04-07 14:26:29 +0900

    Update ExceptionHandlerExceptionResolverCore

--- service/src/main/java/com/github/hotire/springcore/exception/ExceptionHandlerExceptionResolverCore.java
+++ service/src/main/java/com/github/hotire/springcore/exception/ExceptionHandlerExceptionResolverCore.java
@@ -3,9 +3,12 @@ package com.github.hotire.springcore.exception;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.lang.Nullable;
 import org.springframework.web.method.ControllerAdviceBean;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.method.annotation.ExceptionHandlerMethodResolver;
 import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
+import org.springframework.web.servlet.mvc.method.annotation.ServletInvocableHandlerMethod;
 
 /**
  * @see ExceptionHandlerExceptionResolver
@@ -16,4 +19,15 @@ public class ExceptionHandlerExceptionResolverCore {
 
     private final Map<ControllerAdviceBean, ExceptionHandlerMethodResolver> exceptionHandlerAdviceCache =
         new LinkedHashMap<>();
+
+    /**
+     * @see ExceptionHandlerExceptionResolver#getExceptionHandlerMethod(HandlerMethod, Exception)
+     * @param handlerMethod
+     * @param exception
+     * @return
+     */
+    protected ServletInvocableHandlerMethod getExceptionHandlerMethod(
+        @Nullable HandlerMethod handlerMethod, Exception exception) {
+        return null;
+    }
 }
