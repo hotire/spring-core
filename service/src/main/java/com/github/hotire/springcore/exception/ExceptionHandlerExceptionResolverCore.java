042058d7145fc47235a9a83714d1e4f6d8b74906
hotire <gngh0101@gmail.com>
2024-04-08 00:37:52 +0900

    Update

--- service/src/main/java/com/github/hotire/springcore/exception/ExceptionHandlerExceptionResolverCore.java
+++ service/src/main/java/com/github/hotire/springcore/exception/ExceptionHandlerExceptionResolverCore.java
@@ -1,8 +1,13 @@
 package com.github.hotire.springcore.exception;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import lombok.RequiredArgsConstructor;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.context.ApplicationContext;
 import org.springframework.lang.Nullable;
 import org.springframework.web.method.ControllerAdviceBean;
 import org.springframework.web.method.HandlerMethod;
@@ -13,12 +18,15 @@ import org.springframework.web.servlet.mvc.method.annotation.ServletInvocableHan
 /**
  * @see ExceptionHandlerExceptionResolver
  */
+@RequiredArgsConstructor
 public class ExceptionHandlerExceptionResolverCore {
     private final Map<Class<?>, ExceptionHandlerMethodResolver> exceptionHandlerCache =
         new ConcurrentHashMap<>(64);
 
     private final Map<ControllerAdviceBean, ExceptionHandlerMethodResolver> exceptionHandlerAdviceCache =
         new LinkedHashMap<>();
+
+    private final ApplicationContext applicationContext;
 
     /**
      * @see ExceptionHandlerExceptionResolver#getExceptionHandlerMethod(HandlerMethod, Exception)
@@ -28,6 +36,29 @@ public class ExceptionHandlerExceptionResolverCore {
      */
     protected ServletInvocableHandlerMethod getExceptionHandlerMethod(
         @Nullable HandlerMethod handlerMethod, Exception exception) {
+
+        Class<?> handlerType = null;
+
+        if (handlerMethod != null) {
+            // Local exception handler methods on the controller class itself.
+            // To be invoked through the proxy, even in case of an interface-based proxy.
+            handlerType = handlerMethod.getBeanType();
+            ExceptionHandlerMethodResolver resolver = this.exceptionHandlerCache.get(handlerType);
+            if (resolver == null) {
+                resolver = new ExceptionHandlerMethodResolver(handlerType);
+                this.exceptionHandlerCache.put(handlerType, resolver);
+            }
+            Method method = resolver.resolveMethod(exception);
+            if (method != null) {
+                return new ServletInvocableHandlerMethod(handlerMethod.getBean(), method, this.applicationContext);
+            }
+            // For advice applicability check below (involving base packages, assignable types
+            // and annotation presence), use target class instead of interface-based proxy.
+            if (Proxy.isProxyClass(handlerType)) {
+                handlerType = AopUtils.getTargetClass(handlerMethod.getBean());
+            }
+        }
+
         return null;
     }
 }
