91f644af6dcddf9f2dd5c0347031c8eb53f231f3
hotire <gngh0101@gmail.com>
2024-04-15 01:25:32 +0900

    Add ExceptionHandlerMethodResolverCore

---
+++ service/src/main/java/com/github/hotire/springcore/exception/ExceptionHandlerMethodResolverCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.exception;
+
+import org.springframework.web.method.annotation.ExceptionHandlerMethodResolver;
+
+/**
+ * @see ExceptionHandlerMethodResolver
+ */
+public class ExceptionHandlerMethodResolverCore {
+
+}
