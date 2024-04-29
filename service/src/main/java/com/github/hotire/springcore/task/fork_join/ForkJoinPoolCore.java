4200f7a9745faf26124ac83f1ed2b9de96ff6013
hotire <gngh0101@gmail.com>
2024-04-30 02:15:47 +0900

    Add ForkJoinPoolCore

---
+++ service/src/main/java/com/github/hotire/springcore/task/fork_join/ForkJoinPoolCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springcore.task.fork_join;
+
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * @see ForkJoinPool
+ */
+public class ForkJoinPoolCore {
+
+}
---
+++ service/src/main/java/com/github/hotire/springcore/task/fork_join/README.md
@@ -0,0 +1,3 @@
+# ForkJoin
+
+- 기본동작원리 : https://upcurvewave.tistory.com/653
